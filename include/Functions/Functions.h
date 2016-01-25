01a1fa91bf1d5f52018c409cff8aca5404c9f6aa
Mingxu Hu <humingxu@gmail.com>
2016-01-25 14:58:46 +0800

    MKB_FT Done

--- include/Functions/Functions.h
+++ include/Functions/Functions.h
@@ -11,8 +11,12 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include <cmath>
+
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_statistics_double.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_sf_bessel.h>
 
 #include "Functions.h"
 
@@ -25,5 +29,15 @@
 #define MIN_3(a, b, c) MIN(MIN(a, b), c)
 
 void normalise(gsl_vector& vec);
+
+double MKB_FT(const double r,
+              const double a,
+              const double alpha);
+/* Modified Kaiser Bessel Function, m = 2 */
+
+double MKB_RL(const double r,
+              const double a,
+              const double alpha);
+/* Inverse Fourier Transform of Modified Kaiser Bessel Function, m = 2, n = 3 */
 
 #endif // FUNCTIONS_H
--- src/Functions/Functions.cpp
+++ src/Functions/Functions.cpp
@@ -18,3 +18,17 @@ void normalise(gsl_vector& vec)
     gsl_vector_add_constant(&vec, -mean);
     gsl_vector_scale(&vec, 1.0 / stddev);
 }
+
+double MKB_FT(const double r,
+              const double a,
+              const double alpha)
+{
+    double u = r / a;
+
+    if (u > 1)
+        return 0;
+
+    return (1 - gsl_pow_2(u))
+         * gsl_sf_bessel_In(2, alpha * sqrt(1 - gsl_pow_2(u)))
+         / gsl_sf_bessel_In(2, alpha);
+}
