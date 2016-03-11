ec67e6259eca4e492160e6eb27e1662aac3db108
Owen Yu <staryhk@gmail.com>
2016-03-11 18:18:55 +0800

    modify parallel

--- src/Parallel.cpp
+++ src/Parallel.cpp
@@ -17,59 +17,40 @@ void Parallel::init(int argc,
 {
     MPI_Group worldGroup;
     MPI_Group AGroup, BGroup;
-    MPI_Group AWorkGroup, BWorkGroup;
 
     MPI_Comm A, B;
-    MPI_Comm Aworker, Bworker;
     
     MPI_Init(&argc, &argv);
     MPI_Comm_size(_world, &_numProcs);
     MPI_Comm_rank(_world, &_myId);
 
-    int half = _numProcs / 2;
+    int half = (_numProcs - 1) / 2;
     int* a = new int[half];
-    int* b = new int[_numProcs - half];
-    for (int i = 0; i < half; i++)
-        a[i] = i;
-    for (int i = half; i < _numProcs; i++)
-        b[i - half] = half;
+    int* b = new int[_numProcs - 1 - half];
+    int server[1];
 
+    for (int i = 1; i <= half; i++)
+        a[i] = i;
+    for (int i = half + 1; i < _numProcs; i++)
+        b[i - half - 1] = i;
+
+    server[0] = 0;
+
+    
     MPI_Comm_group(_world, &worldGroup);
-    MPI_Group_excl(worldGroup, half, a, &BGroup);
-    MPI_Group_excl(worldGroup, _numProcs - half, b, &AGroup); 
+    MPI_Group_incl(worldGroup, half, a, &AGroup); 
+    MPI_Group_incl(worldGroup, _numProcs - 1 - half, b, &BGroup);
+
     MPI_Comm_create(_world, AGroup, &A);
     MPI_Comm_create(_world, BGroup, &B);
 
-    int aserver[1], bserver[1];
-    aserver[0] = 0;
-    bserver[0] = half;
-
-    MPI_Group_excl(AGroup, 1, aserver, &AWorkGroup);
-    MPI_Group_excl(BGroup, 1, bserver, &BWorkGroup); 
-    MPI_Comm_create(A, AWorkGroup, &Aworker);
-    MPI_Comm_create(B, BWorkGroup, &Bworker);
-
-    if (_myId < half)
+    if (_myId <= half && _myId > 0)
     {
         _partWorld = A;
-        _serverId = 0;
-        if (_myId != _serverId)
-        {
-            _partWorldW = Aworker;
-            MPI_Comm_rank(Aworker, &_workerId);
-            MPI_Comm_size(Aworker, &_numWorkers);
-        }
     }
-    else
+    else if (_myId > half)
     {
         _partWorld = B;
-        _serverId = half;
-        if (_myId != _serverId)
-        {
-            _partWorldW = Bworker;
-            MPI_Comm_rank(Bworker, &_workerId);
-            MPI_Comm_size(Bworker, &_numWorkers);
-        }
     }
 
     delete[] a;
