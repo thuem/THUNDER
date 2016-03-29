832e9ce12552a06a1b6cca211ed55cd8bb7a87c1
Mingxu Hu <humingxu@gmail.com>
2016-03-29 22:47:29 +0800

    Small Fix

--- include/ML/MLOptimiser.h
+++ include/ML/MLOptimiser.h
@@ -33,6 +33,8 @@
 
 #include "MLModel.h"
 
+#define FOR_EACH_2D_IMAGE for (int l = 0; l < _ID.size(); l++)
+
 using namespace std;
 using namespace google;
 
--- include/Particle.h
+++ include/Particle.h
@@ -27,8 +27,6 @@
 
 #define TOP_K 20
 
-#define FOR_EACH_2D_IMAGE for (int l = 0; l < _ID.size(); l++)
-
 using namespace arma;
 
 using namespace std;
