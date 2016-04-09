a7594a469558883e3bff5bedf960206583f68d35
Mingxu Hu <humingxu@gmail.com>
2016-04-09 16:12:52 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -137,7 +137,10 @@ void MLOptimiser::expectation()
                        << ": Resampling Particle " << _ID[l]
                        << " for neff = " << _par[l].neff();
 
-            _par[l].resample(GSL_MAX_INT(_para.m, _par[l].n() / 2));
+            if (_iter < 3)
+                _par[l].resample();
+            else
+                _par[l].resample(GSL_MAX_INT(_para.m, _par[l].n() / 2));
             // _par[l].resample();
             // _par[l].perturb();
         }
