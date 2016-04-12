c04048eac353c8b33c60ac139e5128cf7432b7fb
Mingxu Hu <humingxu@gmail.com>
2016-04-12 11:57:15 +0800

    Debug the Perturbation of Particle

--- testsrc/ParticleTest.cpp
+++ testsrc/ParticleTest.cpp
@@ -16,41 +16,46 @@ int main(int argc, const char* argv[])
 
     Particle particle;
     particle.init(M, 30, 30, &sym);
-    // Particle particle(N, 30, 30, &sym);
+
+    save("Initial.par", particle);
 
     vec4 u;
     double v[4];
 
     double nt = M / 3;
-    /***
     bingham_t B;
     bingham_new_S3(&B, e0, e1, e2, -30, -30, 0);
-    ***/
+
+    char filename[FILE_NAME_LENGTH];
     for (int i = 0; i < atoi(argv[1]); i++)
     {
+        printf("Round %04d\n", i);
         // cout << "neff = " << particle.neff() << endl;
         if (particle.neff() < nt)
-            particle.resample(GSL_MAX_INT(100, particle.n() / 2));
+        {
+            particle.resample();
+            //particle.resample(GSL_MAX_INT(100, particle.n() / 2));
+        }
         else particle.perturb();
 
         for (int j = 0; j < M; j++)
         {
+            /***
+            particle.mulW(1, j);
+            ***/
             particle.quaternion(u, j);
             v[0] = u(0);
             v[1] = u(1);
             v[2] = u(2);
             v[3] = u(3);
-            // particle.setW(particle.w(j) * bingham_pdf(v, &B), j);
-            particle.mulW(1, j);
+            particle.mulW(bingham_pdf(v, &B), j);
         }
 
         particle.normW();
-        particle.calVari();
+
+        sprintf(filename, "Round_%04d.par", i);
+        save(filename, particle);
     }
 
-    /***
     bingham_free(&B);
-    ***/
-
-    display(particle);
 }
