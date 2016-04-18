3651e20f6ae556ac0a996bb0e8f361456b30d241
Mingxu Hu <humingxu@gmail.com>
2016-04-18 16:09:00 +0800

    Big Change in Strategy of Resample in MLOptimiser

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -131,20 +131,35 @@ void MLOptimiser::expectation()
                            << ": Resampling Particle " << _ID[l]
                            << " for neff = " << _par[l].neff();
 
-                if (_iter < N_ITER_GLOBAL_SEARCH)
+                if (_iter < N_ITER_TOTAL_GLOBAL_SEARCH)
+                {
+                    if (phase == 0)
+                        _par[l].resample(1);
+                    else
+                        _par[l].resample(ALPHA_GLOBAL_SEARCH_BG);
+                }
+                else if (_iter < N_ITER_TOTAL_GLOBAL_SEARCH
+                               + N_ITER_PARTIAL_GLOBAL_SEARCH)
                 {
                     if (phase == 0)
                         _par[l].resample((ALPHA_GLOBAL_SEARCH_MAX
                                         - ALPHA_GLOBAL_SEARCH_MIN)
-                                       * (N_ITER_GLOBAL_SEARCH - _iter - 1)
-                                       / (N_ITER_GLOBAL_SEARCH - 1)
+                                       * (N_ITER_TOTAL_GLOBAL_SEARCH 
+                                        + N_ITER_PARTIAL_GLOBAL_SEARCH
+                                        - _iter - 1)
+                                       / (N_ITER_PARTIAL_GLOBAL_SEARCH - 1)
                                        + ALPHA_GLOBAL_SEARCH_MIN);
                     else
-                        _par[l].resample(ALPHA_GLOBAL_SEARCH_MIN);
+                        _par[l].resample(ALPHA_GLOBAL_SEARCH_BG);
                 }
                 else
-                    _par[l].resample(GSL_MAX_INT(_para.m, _par[l].n() / 2),
-                                     ALPHA_GLOBAL_SEARCH_MIN);
+                {
+                    if (phase == 0)
+                        _par[l].resample(GSL_MAX_INT(_para.m, _par[l].n() / 2),
+                                         ALPHA_GLOBAL_SEARCH_BG);
+                    else
+                        _par[l].resample();
+                }
             }
 
             int nSearch = 0;
