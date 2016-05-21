d9af5bab49fad4b85520eeb81f1d1b9b02895f8c
Mingxu Hu <humingxu@gmail.com>
2016-05-21 15:54:46 +0800

    More

--- src/ML/MLModel.cpp
+++ src/ML/MLModel.cpp
@@ -282,7 +282,7 @@ void MLModel::updateR()
     FOR_EACH_CLASS
         if (_FSC.col(i)(_pf * _r - 1) > 0.2)
         {
-            _r += MIN(MAX_GAP, AROUND(double(_size) / 8));
+            _r += MIN(MAX_GAP, AROUND(double(_size) / 16));
             _r = MIN(_r, _size / 2 - _a);
             return;
         }
