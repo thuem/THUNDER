9b83f9f57c39b4fdbe40dc740fb798cb92239c0d
Mingxu Hu <humingxu@gmail.com>
2016-05-21 16:12:28 +0800

    More

--- casesrc/case1/MickeyMouseDataGen.cpp
+++ casesrc/case1/MickeyMouseDataGen.cpp
@@ -125,7 +125,7 @@ int main(int argc, char* argv[])
         CS);
 
     cout << "Initialising Experiment" << endl;
-    Experiment exp("/tmp/MickeyMouse.db");
+    Experiment exp("MickeyMouse.db");
     exp.createTables();
     exp.appendMicrograph("", VOLTAGE, DEFOCUS_U, DEFOCUS_V, THETA, CS);
     exp.appendGroup("");
