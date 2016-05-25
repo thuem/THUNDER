70337c7add936a87195280e8db477c5c3748627a
Mingxu Hu <humingxu@gmail.com>
2016-05-25 17:13:20 +0800

    More

--- casesrc/case1/InitModelGen.cpp
+++ casesrc/case1/InitModelGen.cpp
@@ -22,6 +22,7 @@ int main(int argc, char* argv[])
     Volume sphere(N, N, N, RL_SPACE);
     VOLUME_FOR_EACH_PIXEL_RL(sphere)
     {
+        /***
         double ii = i * 0.8;
         double jj = j * 0.8;
         double kk = k * 0.8;
@@ -29,7 +30,7 @@ int main(int argc, char* argv[])
             sphere.setRL(1, i, j, k);
         else
             sphere.setRL(0, i, j, k);
-        /***
+        ***/
         if ((NORM_3(ii, jj, kk) < N / 8) ||
             (NORM_3(ii - N / 8, jj, kk - N / 8) < N / 16) ||
             (NORM_3(ii + N / 8, jj, kk - N / 8) < N / 16) ||
@@ -39,7 +40,6 @@ int main(int argc, char* argv[])
             sphere.setRL(1, i, j, k);
         else
             sphere.setRL(0, i, j, k);
-        ***/
     }
 
     Volume padSphere;
