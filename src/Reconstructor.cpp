0cbac41bd18b4166a70788234326bc7e71ac9ec1
Mingxu Hu <humingxu@gmail.com>
2016-05-27 16:00:39 +0800

    Extreme Severely Bug Fixed

--- src/Reconstructor.cpp
+++ src/Reconstructor.cpp
@@ -233,7 +233,7 @@ void Reconstructor::allReduceW()
 
     #pragma omp parallel for schedule(dynamic)
     VOLUME_FOR_EACH_PIXEL_FT(_W)
-        if (QUAD_3(i, j, k) < gsl_pow_3(_maxRadius))
+        if (QUAD_3(i, j, k) < gsl_pow_2(_maxRadius))
         {
             double c = REAL(_C.getFT(i, j, k, conjugateNo));
             _W.setFT(2 * c * _W.getFT(i, j, k, conjugateNo) / (1 + gsl_pow_2(c)),
