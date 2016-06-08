0af31586c531a3b29702f26c465022b45c41e9df
Mingxu Hu <humingxu@gmail.com>
2016-06-08 17:01:02 +0800

    More

--- src/Parallel.cpp
+++ src/Parallel.cpp
@@ -142,12 +142,10 @@ void MPI_Recv_Large(void* buf,
 
     int nBlock = (count - 1) / (MPI_MAX_BUF / dataTypeSize) + 1;
 
-    /***
     if (nBlock != 1)
         CLOG(INFO, "LOGGER_MPI") << "MPI_Recv_Large: Transmitting "
                                  << nBlock
                                  << " Block(s).";
-    ***/
 
     char* ptr = static_cast<char*>(buf);
 
@@ -189,12 +187,10 @@ void MPI_Ssend_Large(const void* buf,
 
     int nBlock = (count - 1) / (MPI_MAX_BUF / dataTypeSize) + 1;
 
-    /***
     if (nBlock != 1)
         CLOG(INFO, "LOGGER_MPI") << "MPI_Ssend_Large: Transmitting "
                                  << nBlock
                                  << " Block(s).";
-    ***/
 
     const char* ptr = static_cast<const char*>(buf);
 
@@ -227,12 +223,10 @@ void MPI_Bcast_Large(void* buf,
 
     int nBlock = (count - 1) / (MPI_MAX_BUF / dataTypeSize) + 1;
 
-    /***
     if (nBlock != 1)
         CLOG(INFO, "LOGGER_MPI") << "MPI_Bcast_Large: Transmitting "
                                  << nBlock
                                  << " Block(s).";
-    ***/
 
     char* ptr = static_cast<char*>(buf);
 
@@ -245,11 +239,9 @@ void MPI_Bcast_Large(void* buf,
 
         MPI_Barrier(comm);
 
-        /***
         if (nBlock != 1)
             CLOG(INFO, "LOGGER_MPI") << "MPI_Bcast_Large: Transmitting Block "
                                      << i;
-        ***/
 
         MPI_Bcast(ptr,
                   blockSize,
@@ -293,12 +285,10 @@ void MPI_Allreduce_Large(void* buf,
     //CLOG(INFO, "LOGGER_MPI") << "MPI_MAX_BUF = " << MPI_MAX_BUF;
     //CLOG(INFO, "LOGGER_MPI") << "dataTypeSize = " << dataTypeSize;
 
-    /***
     if (nBlock != 1)
         CLOG(INFO, "LOGGER_MPI") << "MPI_Allreduce_Large: Transmitting "
                                  << nBlock
                                  << " Block(s).";
-    ***/
 
     char* ptr = static_cast<char*>(buf);
 
@@ -311,31 +301,9 @@ void MPI_Allreduce_Large(void* buf,
 
         MPI_Barrier(comm);
         
-        /***
         if (nBlock != 1)
             CLOG(INFO, "LOGGER_MPI") << "MPI_Allreduce_Large: Transmitting Block "
                                      << i;
-        ***/
-
-        /***
-        if (sendbuf != MPI_IN_PLACE)
-            MPI_Allreduce(static_cast<const char*>(sendbuf) + i * MPI_MAX_BUF,
-                          static_cast<char*>(recvbuf) + i * MPI_MAX_BUF,
-                          blockSize,
-                          datatype,
-                          op,
-                          comm);
-                          ***/
-            //CLOG(INFO, "LOGGER_MPI") << "MPI_Allreduce_Large: In Place Allreduce";
-            /***
-            MPI_Allreduce(MPI_IN_PLACE,
-                          static_cast<char*>(recvbuf) + i * MPI_MAX_BUF,
-                          blockSize,
-                          datatype,
-                          op,
-                          comm);
-                          ***/
-            //cout << (long long)ptr << endl;
 
         MPI_Allreduce(MPI_IN_PLACE,
                       ptr,
