ae0e833490d37b64d2decc7125a5a652273c06d1
Mingxu Hu <humingxu@gmail.com>
2016-07-16 21:01:57 +0800

    More

--- casesrc/case2/DataGen.cpp
+++ casesrc/case2/DataGen.cpp
@@ -168,7 +168,6 @@ int main(int argc, char* argv[])
         FOR_EACH_PIXEL_FT(image)
             image[i] *= REAL(ctf[i]);
 
-        /***
         Image noise(N, N, FT_SPACE);
         SET_0_FT(noise);
         IMAGE_FOR_EACH_PIXEL_FT(noise)
@@ -179,7 +178,6 @@ int main(int argc, char* argv[])
                             j);
 
         ADD_FT(image, noise);
-        ***/
 
         fftThread.bw(image);
 
