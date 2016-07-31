ef35d7781d151c48e7d33bd3b47f6766e04689a7
Mingxu Hu <humingxu@gmail.com>
2016-07-31 16:21:30 +0800

    More

--- src/ML/MLModel.cpp
+++ src/ML/MLModel.cpp
@@ -184,11 +184,13 @@ void MLModel::BcastFSC()
 
             MLOG(INFO, "LOGGER_COMPARE") << "Averaging A and B Below a Certain Resolution";
 
-            double r = MIN((resA2P(1.0 / A_B_AVERAGE_THRES, _size, _pixelSize) + 1) * _pf,
-                           0.5 * (_r + 1) * _pf);
+            double r = GSL_MIN_DBL((resA2P(1.0 / A_B_AVERAGE_THRES,
+                                           _size,
+                                           _pixelSize) + 1) * _pf,
+                                   0.5 * (_r + 1) * _pf);
 
             MLOG(INFO, "LOGGER_COMPARE") << "Averaging A and B Belower Resolution "
-                                         << resP2A(r / _pf - 1, _size, _pixelSize);
+                                         << 1.0 / resP2A(r / _pf - 1, _size, _pixelSize);
 
             #pragma omp parallel for schedule(dynamic)
             VOLUME_FOR_EACH_PIXEL_FT(A)
