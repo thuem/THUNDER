b75e8bffbe8ebce837c7f80749d7e86ebe41a87e
Mingxu Hu <humingxu@gmail.com>
2016-08-02 16:31:21 +0800

    More

--- src/Projector.cpp
+++ src/Projector.cpp
@@ -67,10 +67,8 @@ void Projector::setProjectee(Volume src)
                              _projectee.nRowRL(),
                              _projectee.nSlcRL()) / _pf / 2 - 1);
 
-    /***
     // perform grid correction
     gridCorrection();
-    ***/
 }
 
 void Projector::project(Image& dst,
