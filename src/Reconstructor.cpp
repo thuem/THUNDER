294ddc40989d2321570f4c195715cd422e0ef160
Mingxu Hu <humingxu@gmail.com>
2016-08-07 21:29:45 +0800

    More

--- src/Reconstructor.cpp
+++ src/Reconstructor.cpp
@@ -177,12 +177,14 @@ void Reconstructor::reconstruct(Volume& dst)
             (dst.getRL(i, j, k) > 0))
         ***/
         {
+            /***
             dst.setRL(dst.getRL(i, j, k)
                     / MKB_RL(r, _pf * _a, _alpha)
                     / TIK_RL(r),
                       i,
                       j,
                       k);
+            ***/
         }
         else
             dst.setRL(0, i, j, k);
