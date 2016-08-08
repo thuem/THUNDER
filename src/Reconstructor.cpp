37367920f9bd30bb024e2586662851d00a0e862f
Mingxu Hu <humingxu@gmail.com>
2016-08-08 23:26:18 +0800

    More

--- src/Reconstructor.cpp
+++ src/Reconstructor.cpp
@@ -177,7 +177,7 @@ void Reconstructor::reconstruct(Volume& dst)
             dst.setRL(0, i, j, k);
         ***/
 
-        if ((r < 0.25 / _pf) &&
+        if ((r < 0.5 / _pf) &&
             (dst.getRL(i, j, k) > 0))
         {
             dst.setRL(dst.getRL(i, j, k)
