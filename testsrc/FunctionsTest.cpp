abc990a4d98f13a874db14c6e23283d66868ffc5
Mingxu Hu <humingxu@gmail.com>
2016-08-11 20:27:10 +0800

    More

--- testsrc/FunctionsTest.cpp
+++ testsrc/FunctionsTest.cpp
@@ -10,16 +10,26 @@
 
 #include "Functions.h"
 
+#define BLOB_A 1.9
+#define BLOB_ALPHA 5
+
+#define PF 2
+
 using namespace std;
 
 int main(int argc, const char* argv[])
 {
     // cout << atoi(argv[1]) << endl;
+    /***
     for (double i = 0; i <= 2.5; i += 0.01)
         cout << i << " " << MKB_FT(i, 2, atoi(argv[1])) << endl;
+    ***/
 
     /***
     for (double i = 0; i <= 1.5; i += 0.01)
         cout << i << " " << MKB_RL(i, 2, 0.5) << endl;
     ***/
+    
+    for (double i = 0; i <= BLOB_A * PF; i += 0.01)
+        cout << i << " " << MKB_RL(i, PF * BLOB_A, BLOB_ALPHA);
 }
