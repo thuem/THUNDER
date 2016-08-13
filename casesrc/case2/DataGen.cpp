ad417d5efd90eb3596e552740b2880ddbf4f3606
Mingxu Hu <humingxu@gmail.com>
2016-08-13 22:38:10 +0800

    More

--- casesrc/case2/DataGen.cpp
+++ casesrc/case2/DataGen.cpp
@@ -205,13 +205,11 @@ int main(int argc, char* argv[])
 
         fftThread.bw(image);
 
-        /***
         Image noise(N, N, RL_SPACE);
         FOR_EACH_PIXEL_RL(noise)
-            noise(i) = gsl_ran_gaussian(engine, std);
+            noise(i) = gsl_ran_gaussian(engine, 2 * std);
 
         ADD_RL(image, noise);
-        ***/
 
         /***
         printf("image: mean = %f, stddev = %f, maxValue = %f\n",
