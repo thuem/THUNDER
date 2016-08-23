b8e04e3279f4952ca87b2e50635925ee635561de
Mingxu Hu <humingxu@gmail.com>
2016-08-23 19:10:37 +0800

    Comments on CTF Done

--- include/CTF.h
+++ include/CTF.h
@@ -21,6 +21,17 @@
 #define CTF_TAU 0.01
 //#define CTF_TAU 0.1
 
+/**
+ * This function generates a CTF using parameters given.
+ *
+ * @param dst the destination image
+ * @param pixelSize pixel size (Angstrom)
+ * @param voltage voltage (Volt)
+ * @param defocusU the first defocus parameter (Angstrom)
+ * @param defocusV the second defocus parameter (Angstrom)
+ * @param theta the defocus angle (rad)
+ * @param Cs Cs
+ */
 void CTF(Image& dst,
          const double pixelSize,
          const double voltage,
@@ -28,24 +39,34 @@ void CTF(Image& dst,
          const double defocusV,
          const double theta,
          const double Cs);
-/* pixelSize : Angstrom
- * voltage : V 
- * defocusU : Angstrom
- * defocusV : Angstrom
- * theta : rad
- * Cs */
 
+/**
+ * This function reduces the CTF affect using Wiener filter.
+ *
+ * @param dst the destination image
+ * @param src the source image
+ * @param ctf CTF
+ */
 void reduceCTF(Image& dst,
                const Image& src,
                const Image& ctf);
 
+/**
+ * This function reduces the CTF affect using Wiener filter below a certain
+ * frequency.
+ *
+ * @param dst the destination image
+ * @param src the source image
+ * @param ctf CTF
+ * @param r the frequency threshold
+ */
 void reduceCTF(Image& dst,
                const Image& src,
                const Image& ctf,
                const int r);
 
 /**
- * This function reduce the CTF affect using Wiener filter. Sigma stands for the
+ * This function reduces the CTF affect using Wiener filter. Sigma stands for the
  * power spectrum of the noise, meanwhile tau stands for the power spectrum of
  * the signal. It is worth noticed that tau and sigma should be in the same
  * scale.
