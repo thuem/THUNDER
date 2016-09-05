317cbe66bbb7fc4b0d4887c9b19c3fedc588da24
Mingxu Hu <humingxu@gmail.com>
2016-09-05 15:23:29 +0800

    Comment of PointGroup Changed

--- include/Geometry/PointGroup.h
+++ include/Geometry/PointGroup.h
@@ -40,71 +40,101 @@
 #define PG_I4H  224
 #define PG_I5H  225
 
+/**
+ * 3-fold Axis 1 <--> 3-fold Axis 2
+ */
 static const double pg_T_a1[] = {-0.942809,
                                  0,
                                  0};
-/* 3-fold Axis 1 <--> 3-fold Axis 2 */
 
+/**
+ * 3-fold Axis 2 <--> 3-fold Axis 3
+ */
 static const double pg_T_a2[] = {0.471405,
                                  0.272165,
                                  0.7698};
-/* 3-fold Axis 2 <--> 3-fold Axis 3 */
 
+/**
+ * 3-fold Axis 3 <--> 3-fold Axis 1
+ */
 static const double pg_T_a3[] = {0.471405,
                                  0.816497,
                                  0};
-/* 3-fold Axis 3 <--> 3-fold Axis 1 */
 
+/**
+ * 2-fold Axis 1 <--> 3-fold Axis 2
+ */
 static const double pg_TD_a1[] = {-0.942809,
                                   0,
                                   0};
-/* 2-fold Axis 1 <--> 3-fold Axis 2 */
 
+/**
+ * 3-fold Axis 2 <--> 3-fold Axis 5
+ */
 static const double pg_TD_a2[] = {0.471405,
                                   0.272165,
                                   0.7698};
-/* 3-fold Axis 2 <--> 3-fold Axis 5 */
 
+/**
+ * 3-fold Axis 5 <--> 2-fold Axis 1
+ */
 static const double pg_TD_a3[] = {0,
                                   0.471405,
                                   -0.666667};
-/* 3-fold Axis 5 <--> 2-fold Axis 1 */
 
+/**
+ * 3-fold Axis 1 <--> 2-fold Axis 1
+ */
 static const double pg_TH_a1[] = {-0.816496,
                                   0,
                                   0};
-/* 3-fold Axis 1 <--> 2-fold Axis 1 */
 
+/**
+ * 2-fold Axis 1 <--> 2-fold Axis 2
+ */
 static const double pg_TH_a2[] = {0.707107,
                                   0.408248,
                                   -0.57735};
-/* 2-fold Axis 1 <--> 2-fold Axis 2 */
 
+/**
+ * 2-fold Axis 2 <--> 3-fold Axis 1
+ */
 static const double pg_TH_a3[] = {-0.408248,
                                   -0.707107,
                                   0};
-/* 2-fold Axis 2 <--> 3-fold Axis 1 */
 
+/**
+ * 3-fold Axis 1 <--> 3-fold Axis 2
+ */
 static const double pg_O_a1[] = {0, -1, 1};
-/* 3-fold Axis 1 <--> 3-fold Axis 2 */
 
+/**
+ * 3-fold Axis 1 <--> 4-fold Axis
+ */
 static const double pg_O_a2[] = {1, 1, 0};
-/* 3-fold Axis 1 <--> 4-fold Axis */
 
+/**
+ * 4-fold Axis <--> 3-fold Axis 1
+ */
 static const double pg_O_a3[] = {-1, 1, 0};
-/* 4-fold Axis <--> 3-fold Axis 1 */
 
+/**
+ * 5-fold Axis 1 <--> 5-fold Axis 2
+ */
 static const double pg_I2_a1[] = {0, 1, 0};
-/* 5-fold Axis 1 <--> 5-fold Axis 2 */
 
+/**
+ * 5-fold Axis 2 <--> 3-fold Axis
+ */
 static const double pg_I2_a2[] = {-0.5,
                                   -0.809017007,
                                   0.309016986};
-/* 5-fold Axis 2 <--> 3-fold Axis */
 
+/**
+ * 3-fold Axis <--> 5-fold Axis 1
+ */
 static const double pg_I2_a3[] = {0.5,
                                   -0.809017007,
                                   0.309016986}; 
-/* 3-fold Axis <--> 5-fold Axis 1 */
 
 #endif // POINT_GROUP_H
