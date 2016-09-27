5c1dfeb9c453a02062d9c7186c833d41e43a1d76
Mingxu Hu <humingxu@gmail.com>
2016-09-27 17:40:10 +0800

    More

--- casesrc/case2/Run.cpp
+++ casesrc/case2/Run.cpp
@@ -50,7 +50,7 @@ int main(int argc, char* argv[])
     para.mG = MG;
     para.mL = ML;
     para.transS = TRANS_S;
-    para.initRes = 60;
+    para.initRes = 40;
     para.ignoreRes = 200;
     sprintf(para.sym, "C15");
     // sprintf(para.initModel, "padCylinder.mrc");
