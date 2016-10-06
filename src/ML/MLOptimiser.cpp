49f9a68c60668b25fe086f5296d9850d43289e0f
Mingxu Hu <humingxu@gmail.com>
2016-10-06 16:14:06 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -1860,10 +1860,8 @@ void MLOptimiser::saveImages()
     {
         if (_ID[l] < N_SAVE_IMG)
         {
-            /***
             sprintf(filename, "Fourier_Image_%04d.bmp", _ID[l]);
             _img[l].saveFTToBMP(filename, 0.01);
-            ***/
 
             sprintf(filename, "Image_%04d.bmp", _ID[l]);
 
