2d4d1d89a81f7b9da0a1d1479de6f5ba17f5dbc7
Mingxu Hu <humingxu@gmail.com>
2016-10-12 20:13:12 +0800

    More

--- Python_STAR/gen.py
+++ Python_STAR/gen.py
@@ -1,29 +1,43 @@
 import sqlite3
+import math
 
-lines = open("main.star", "r").readlines()
-# print lines
+fSTAR = "main.star"
+
+fDB = "C15.db"
+
+iVoltage = 0
+
+iDefocusU = 1
+
+iDefocusV = 2
+
+iDefocusTheta = 3
+
+iCs = 4
+
+iImageName = 9
+
+iMacrographName = 13
+
+lines = open(fSTAR, "r").readlines()
 
 print "There is ", len(lines), " particles in total."
 
-voltage = [float(line.strip().split()[0]) for line in lines]
-# print voltage
+voltage = [float(line.strip().split()[iVoltage]) * 1000 for line in lines]
 
-defocusU = [float(line.strip().split()[1]) for line in lines]
-# print defocusU
+defocusU = [float(line.strip().split()[iDefocusU]) for line in lines]
 
-defocusV = [float(line.strip().split()[2]) for line in lines]
-# print defocusU
+defocusV = [float(line.strip().split()[iDefocusV]) for line in lines]
 
-defocusTheta = [float(line.strip().split()[3]) for line in lines]
-# print defocusTheta
+defocusTheta = [float(line.strip().split()[iDefocusTheta]) / 180 * math.pi for line in lines]
 
-imageName = [line.strip().split()[9] for line in lines]
-# print imageName
+CS = [float(line.strip().split()[iCs]) for line in lines]
 
-micrographName = [line.strip().split()[13] for line in lines]
-# print micrographName
+imageName = [line.strip().split()[iImageName] for line in lines]
 
-conn = sqlite3.connect("C15.db")
+micrographName = [line.strip().split()[iMacrographName] for line in lines]
+
+conn = sqlite3.connect(fDB)
 
 print "Open Database Successfully"
 
@@ -48,12 +62,6 @@ conn.execute('''CREATE TABLE groups
 
 conn.commit()
 
-sql = "INSERT INTO groups (ID, Name) VALUES (1, '')"
-
-conn.execute(sql)
-
-conn.commit()
-
 print "Tables Created"
 
 micrographList = []
@@ -68,6 +76,7 @@ for i in range(len(lines)):
         micrographID = len(micrographList) + 1
 
         micrographList.append(micrographName[i])
+
         sql = "INSERT INTO micrographs (ID, Name, Voltage, DefocusU, \
                DefocusV, DefocusAngle, Cs) VALUES (" \
             + str(micrographID) + ", '" \
@@ -75,8 +84,20 @@ for i in range(len(lines)):
             + str(voltage[i]) + ", " \
             + str(defocusU[i]) + ", " \
             + str(defocusV[i]) + ", " \
-            + str(defocusTheta[i]) + ", " + "0);"
+            + str(defocusTheta[i]) + ", " \
+            + str(CS[i]) + ");"
+
         conn.execute(sql)
+
+        conn.commit()
+
+        sql = "INSERT INTO groups (ID, Name) VALUES (" \
+            + str(micrographID) \
+            + ", '');"
+
+        conn.execute(sql)
+
+        conn.commit()
 
         # groupID = micrographID
 
@@ -84,23 +105,22 @@ for i in range(len(lines)):
           #   + str(groupID) + ", '')"
         # conn.execute(sql)
 
-        # conn.commit()
-
     else:
 
         micrographID = micrographList.index(micrographName[i]) + 1
 
         # groupID = micrographID
 
-    groupID = 1
+    #groupID = 1
 
     sql = "INSERT INTO particles (ID, Name, GroupID, MicrographID) VALUES (" \
         + str(i + 1) + ", '" \
         + imageName[i] + "', " \
-        + str(groupID) + ", " \
+        + str(micrographID) + ", " \
         + str(micrographID) +");";
 
     conn.execute(sql)
+
     conn.commit()
 
 #print micrographList
