019fc426c87829d30b4dee8bf952fa9e52b9ea09
Mingxu Hu <humingxu@gmail.com>
2016-10-22 11:47:35 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -272,7 +272,7 @@ void MLOptimiser::expectation()
 
         _nR = 0;
 
-        #pragma omp parallel for schedule(dynamic) private(rot)
+        //#pragma omp parallel for schedule(dynamic) private(rot)
         for (unsigned int m = 0; m < (unsigned int)nR; m++)
         {
             Image imgRot(size(), size(), FT_SPACE);
@@ -282,8 +282,8 @@ void MLOptimiser::expectation()
 
             par.rot(rot, m * nT);
 
-            _model.proj(0).project(imgRot, rot);
-            //_model.proj(0).projectMT(imgRot, rot);
+            //_model.proj(0).project(imgRot, rot);
+            _model.proj(0).projectMT(imgRot, rot);
 
             for (unsigned int n = 0; n < (unsigned int)nT; n++)
             {
@@ -294,7 +294,7 @@ void MLOptimiser::expectation()
                 IMAGE_FOR_EACH_PIXEL_FT(imgAll)
                 ***/
 
-                //#pragma omp parallel for schedule(dynamic)
+                #pragma omp parallel for schedule(dynamic)
                 IMAGE_FOR_PIXEL_R_FT(_r)
                 {
                     if (QUAD(i, j) < gsl_pow_2(_r))
@@ -336,7 +336,7 @@ void MLOptimiser::expectation()
                 //#pragma omp parallel for
                 FOR_EACH_2D_IMAGE
                 {
-                    #pragma omp critical
+                    //#pragma omp critical
                     recordTopK(topW.col(l).data(),
                                iTopR.col(l).data(),
                                iTopT.col(l).data(),
@@ -360,10 +360,10 @@ void MLOptimiser::expectation()
                 ***/
             }
 
-            #pragma omp atomic
+            //#pragma omp atomic
             _nR += 1;
 
-            #pragma omp critical
+            //#pragma omp critical
             if (_nR > (int)(nR / 10))
             {
                 _nR = 0;
@@ -2482,7 +2482,7 @@ vec logDataVSPrior(const vector<Image>& dat,
             {
                 int index = dat[0].iFTHalf(i, j);
 
-                //#pragma omp parallel for
+                #pragma omp parallel for
                 for (int l = 0; l < n; l++)
                 {
                     result(l) += ABS2(dat[l].iGetFT(index)
@@ -2510,7 +2510,7 @@ vec logDataVSPrior(const vector<Image>& dat,
 
     vec result = vec::Zero(n);
 
-    //#pragma omp parallel for
+    #pragma omp parallel for
     for (int l = 0; l < n; l++)
     {
         for (int i = 0; i < m; i++)
