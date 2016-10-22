3b1cd391334329973eae0f8fdccda44554721d01
Mingxu Hu <humingxu@gmail.com>
2016-10-22 12:43:19 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -282,8 +282,8 @@ void MLOptimiser::expectation()
 
             par.rot(rot, m * nT);
 
-            _model.proj(0).project(imgRot, rot);
-            //_model.proj(0).projectMT(imgRot, rot);
+            //_model.proj(0).project(imgRot, rot);
+            _model.proj(0).projectMT(imgRot, rot);
 
             for (unsigned int n = 0; n < (unsigned int)nT; n++)
             {
@@ -294,7 +294,7 @@ void MLOptimiser::expectation()
                 IMAGE_FOR_EACH_PIXEL_FT(imgAll)
                 ***/
 
-                //#pragma omp parallel for schedule(dynamic)
+                #pragma omp parallel for schedule(dynamic)
                 IMAGE_FOR_PIXEL_R_FT(_r)
                 {
                     if (QUAD(i, j) < gsl_pow_2(_r))
@@ -333,7 +333,7 @@ void MLOptimiser::expectation()
                                          _iSig,
                                          nPxl);
 
-                //#pragma omp parallel for
+                #pragma omp parallel for
                 FOR_EACH_2D_IMAGE
                 {
                     //#pragma omp critical
@@ -2482,7 +2482,7 @@ vec logDataVSPrior(const vector<Image>& dat,
             {
                 int index = dat[0].iFTHalf(i, j);
 
-                //#pragma omp parallel for
+                #pragma omp parallel for
                 for (int l = 0; l < n; l++)
                 {
                     result(l) += ABS2(dat[l].iGetFT(index)
@@ -2510,7 +2510,7 @@ vec logDataVSPrior(const vector<Image>& dat,
 
     vec result = vec::Zero(n);
 
-    //#pragma omp parallel for
+    #pragma omp parallel for
     for (int l = 0; l < n; l++)
     {
         for (int i = 0; i < m; i++)
@@ -2574,12 +2574,12 @@ void scaleDataVSPrior(vec& sXA,
             {
                 int index = dat.iFTHalf(i, j);
 
-                #pragma omp critical
+                #pragma omp atomic
                 sXA(v) += REAL(dat.iGetFT(index)
                              * pri.iGetFT(index)
                              * REAL(ctf.iGetFT(index)));
 
-                #pragma omp critical
+                #pragma omp atomic
                 sAA(v) += REAL(pri.iGetFT(index)
                              * pri.iGetFT(index)
                              * gsl_pow_2(REAL(ctf.iGetFT(index))));
