e522490a924b765a7ecf838bc477f30387f8feee
Mingxu Hu <humingxu@gmail.com>
2016-11-17 21:38:11 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -441,8 +441,8 @@ void MLOptimiser::expectation()
         {
             vec v = topW.col(l);
 
-            //PROCESS_LOGW_SOFT(v);
-            PROCESS_LOGW_HARD(v);
+            PROCESS_LOGW_SOFT(v);
+            //PROCESS_LOGW_HARD(v);
 
             topW.col(l) = v;
         }
