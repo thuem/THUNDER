c44e7142010e0d252950a5c28d514e06c7ba7d3f
Mingxu Hu <humingxu@gmail.com>
2016-11-19 16:25:18 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -364,17 +364,27 @@ void MLOptimiser::expectation()
                 //#pragma omp parallel for schedule(dynamic)
                 FOR_EACH_2D_IMAGE
                 {
-                    omp_set_lock(&mtx[l]);
+                    while (omp_test_lock(&mtx[l])) {};
 
                     if ((int)leaderBoard[l].size() < _para.mG)
-                        leaderBoard[l].push(Sp(dvp(l), m, n));
+                    {
+                        //leaderBoard[l].push(Sp(dvp(l), m, n));
+                        omp_set_lock(&mtx[l]);
+
+                        leaderBoard[l].emplace(Sp(dvp(l), m, n));
+
+                        omp_unset_lock(&mtx[l]);
+                    }
                     else if (leaderBoard[l].top()._w < dvp(l))
                     {
-                        leaderBoard[l].pop();
-                        leaderBoard[l].push(Sp(dvp(l), m, n));
-                    };
+                        omp_set_lock(&mtx[l]);
 
-                    omp_unset_lock(&mtx[l]);
+                        leaderBoard[l].pop();
+                        //leaderBoard[l].push(Sp(dvp(l), m, n));
+                        leaderBoard[l].emplace(Sp(dvp(l), m, n));
+
+                        omp_unset_lock(&mtx[l]);
+                    };
                     /***
                     //#pragma omp critical
                     recordTopK(topW.col(l).data(),
