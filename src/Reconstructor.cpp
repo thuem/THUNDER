ddc854be418a53170692fe42549281f75483f1c0
Mingxu Hu <humingxu@gmail.com>
2016-12-16 22:22:34 +0800

    More

--- src/Reconstructor.cpp
+++ src/Reconstructor.cpp
@@ -586,7 +586,7 @@ void Reconstructor::allReduceW()
     ALOG(INFO, "LOGGER_RECO") << "Adding Wiener Factor to C";
     BLOG(INFO, "LOGGER_RECO") << "Adding Wiener Factor to C";
 
-    #pragma omp parallel for
+    #pragma omp parallel for schedule(dynamic)
     VOLUME_FOR_EACH_PIXEL_FT(_T)
         if (QUAD_3(i, j, k) < gsl_pow_2(_maxRadius * _pf))
         {
