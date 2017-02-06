8c446330a924fef6c21bfe138d1dc28f62aff1b9
Mingxu Hu <humingxu@gmail.com>
2017-02-06 20:49:11 +0800

    More

--- include/Geometry/Euler.h
+++ include/Geometry/Euler.h
@@ -22,9 +22,10 @@
 /**
  * This function calculates phi and theta given a certain direction indicated by
  * a 3-vector.
- * @param phi phi
+ *
+ * @param phi   phi
  * @param theta theta
- * @param src 3-vector indicating the direction
+ * @param src   3-vector indicating the direction
  */
 void angle(double& phi,
            double& theta,
@@ -32,10 +33,11 @@ void angle(double& phi,
 
 /**
  * This function calculates phi, theta and psi given the rotation matrix.
- * @param phi phi
+ *
+ * @param phi   phi
  * @param theta theta
- * @param psi psi
- * @param src the rotation matrix
+ * @param psi   psi
+ * @param src   the rotation matrix
  */
 void angle(double& phi,
            double& theta,
@@ -45,10 +47,11 @@ void angle(double& phi,
 /**
  * This function calculates phi, theta and psi given the quaternion indicated
  * by a 4-vector.
- * @param phi phi
+ *
+ * @param phi   phi
  * @param theta theta
- * @param psi psi
- * @param src the quaternion
+ * @param psi   psi
+ * @param src   the quaternion
  */
 void angle(double& phi,
            double& theta,
@@ -57,10 +60,11 @@ void angle(double& phi,
 
 /**
  * This function calculate the quaternion given phi, theta and psi.
- * @param dst the quaternion to be calculated
- * @param phi phi
+ *
+ * @param dst   the quaternion to be calculated
+ * @param phi   phi
  * @param theta theta
- * @param psi psi
+ * @param psi   psi
  */
 void quaternoin(vec4& dst,
                 const double phi,
@@ -68,7 +72,16 @@ void quaternoin(vec4& dst,
                 const double psi);
 
 /**
+ * This function calculates the rotation matrix given the a unit vector.
+ *
+ * @param dst the rotation matrix
+ * @param vec the unit vector
+ */
+void rotate2D(mat22& dst, const vec2& vec);
+
+/**
  * This function calculates the rotation matrix given phi in 2D.
+ *
  * @param dst the rotation matrix
  * @param phi phi
  */
@@ -77,9 +90,10 @@ void rotate2D(mat22& dst, const double phi);
 /**
  * This function calculates the direction vector given phi and theta. The 2-norm
  * of this direction vector is 1.
- * @param dst the direction vector
- * @param phi phi
- * @param theta
+ *
+ * @param dst   the direction vector
+ * @param phi   phi
+ * @param theta theta
  */
 void direction(vec3& dst,
                const double phi,
@@ -87,10 +101,11 @@ void direction(vec3& dst,
 
 /**
  * This function calculates the rotation matrix given phi, theta and psi.
- * @param dst the rotation matrix
- * @param phi phi
+ *
+ * @param dst   the rotation matrix
+ * @param phi   phi
  * @param theta theta
- * @param psi psi
+ * @param psi   psi
  */
 void rotate3D(mat33& dst,
               const double phi,
@@ -99,6 +114,7 @@ void rotate3D(mat33& dst,
 
 /**
  * This function calculates the rotation matrix given a quaternion.
+ *
  * @param dst the rotation matrix
  * @param src the quaternion
  */
@@ -107,6 +123,7 @@ void rotate3D(mat33& dst,
 
 /**
  * This function calculates the rotation matrix of rotation along X-axis of phi.
+ *
  * @param dst the rotation matrix
  * @param phi phi
  */
@@ -114,6 +131,7 @@ void rotate3DX(mat33& dst, const double phi);
 
 /**
  * This function calculates the rotation matrix of rotation along Y-axis of phi.
+ *
  * @param dst the rotation matrix
  * @param phi phi
  */
@@ -121,6 +139,7 @@ void rotate3DY(mat33& dst, const double phi);
 
 /**
  * This function calculates the rotation matrix of rotation along Z-axis of phi.
+ *
  * @param dst the rotation matrix
  * @param phi phi
  */
@@ -129,6 +148,7 @@ void rotate3DZ(mat33& dst, const double phi);
 /**
  * This function calculates the rotation matrix for aligning a direction vector
  * to Z-axis.
+ *
  * @param dst the rotation matrix
  * @param vec the direction vector
  */
@@ -138,7 +158,8 @@ void alignZ(mat33& dst,
 /**
  * This function calculates the rotation matrix of rotation along a certain axis
  * (X, Y or Z) of phi.
- * @param dst the rotation matrix
+ *
+ * @param dst  the rotation matrix
  * @param axis a character indicating which axis the rotation is along
  */
 void rotate3D(mat33& dst,
@@ -148,8 +169,9 @@ void rotate3D(mat33& dst,
 /**
  * This function calculates the rotation matrix of rotation along a certain axis
  * given by a direction vector of phi.
- * @param dst the rotation matrix
- * @param phi phi
+ *
+ * @param dst  the rotation matrix
+ * @param phi  phi
  * @param axis the direction vector indicating the axis
  */
 void rotate3D(mat33& dst,
@@ -159,7 +181,8 @@ void rotate3D(mat33& dst,
 /**
  * This function calculates the transformation matrix of reflection against a
  * certain plane given by its normal vector.
- * @param dst the rotation matrix
+ *
+ * @param dst   the rotation matrix
  * @param plane the normal vector the reflection plane
  */
 void reflect3D(mat33& dst,
@@ -168,6 +191,7 @@ void reflect3D(mat33& dst,
 /**
  * This function calculates the singular matrix of translation of a certain
  * vector.
+ *
  * @param dst the singular matrix
  * @param vec the translation vector
  */
@@ -176,16 +200,23 @@ void translate3D(mat44& dst,
 
 /**
  * This function calculates the transformation matrix of scaling.
+ *
  * @param dst the transformation matrix
  * @param vec a 3-vector of which vec[0] indicates the scale factor along X
- * axis, vec[1] indicates the scale factor along Y axis and vec[2] indicates the
- * scale factor along Z axis
+ *            axis, vec[1] indicates the scale factor along Y axis and vec[2]
+ *            indicates the scale factor along Z axis
  */
 void scale3D(mat33& dst,
              const vec3& vec);
 
+/**
+ * This function generates a random unit quaternion.
+ */
 void randQuaternion(vec4& quat);
 
+/**
+ * This function generates a random 3D rotation matrix.
+ */
 void randRotate3D(mat33& rot);
 
 #endif // EULER_H 
