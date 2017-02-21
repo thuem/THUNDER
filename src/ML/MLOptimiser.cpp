eb368159d3257b8d8e54e947d4da1382e84b1606
Mingxu Hu <humingxu@gmail.com>
2017-02-21 13:49:23 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -2552,6 +2552,7 @@ void MLOptimiser::normCorrection()
 
             SET_0_FT(img);
 
+            /***
             if (_para.mode == MODE_2D)
             {
                 _par[l].rank1st(cls, rot2D, tran);
@@ -2583,6 +2584,7 @@ void MLOptimiser::normCorrection()
 
             FOR_EACH_PIXEL_FT(img)
                 img[i] *= REAL(_ctf[l][i]);
+            ***/
 
             /***
 #ifdef OPTIMISER_ADJUST_2D_IMAGE_NOISE_ZERO_MEAN
