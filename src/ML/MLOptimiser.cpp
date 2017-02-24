bb4c71a66a8ff2a9c328930f18e932e260ea21ea
Mingxu Hu <humingxu@gmail.com>
2017-02-24 19:58:09 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -872,7 +872,6 @@ void MLOptimiser::expectation()
 
 void MLOptimiser::maximization()
 {
-    /***
     MLOG(INFO, "LOGGER_ROUND") << "Normalisation Noise";
 
     normCorrection();
@@ -891,7 +890,6 @@ void MLOptimiser::maximization()
 
         correctScale(false, true);
     }
-    ***/
 
     ALOG(INFO, "LOGGER_ROUND") << "Reconstruct Reference";
     BLOG(INFO, "LOGGER_ROUND") << "Reconstruct Reference";
