3d0f5022ed06ddff0485555ff639aa110ddc3c88
Mingxu Hu <humingxu@gmail.com>
2017-03-10 16:24:32 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -111,8 +111,8 @@ void MLOptimiser::init()
                               << " Angstrom will be Ingored during Comparison";
 
                               ***/
-    //_rL = 0;
-    _rL = 3.5;
+    _rL = 0;
+    //_rL = 3.5;
 
     MLOG(INFO, "LOGGER_INIT") << "Information Under "
                               << _rL
