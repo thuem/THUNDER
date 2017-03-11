2b1deb7d07fe92ff75d402d5c30aae6c0c2671c0
Mingxu Hu <humingxu@gmail.com>
2017-03-12 01:01:03 +0800

    More

--- src/CTF.cpp
+++ src/CTF.cpp
@@ -20,7 +20,7 @@ double CTF(const double f,
 
     double ki = -K1 * defocus * gsl_pow_2(f) + K2 * gsl_pow_4(f);
 
-    return w1 * sin(ki) - w2 * cos(ki);
+    return -w1 * sin(ki) + w2 * cos(ki);
 }
 
 void CTF(Image& dst,
@@ -49,62 +49,8 @@ void CTF(Image& dst,
 
         double ki = K1 * defocus * gsl_pow_2(u) + K2 * gsl_pow_4(u);
 
-        dst.setFT(COMPLEX(w1 * sin(ki) - w2 * cos(ki), 0),
+        dst.setFT(COMPLEX(-w1 * sin(ki) + w2 * cos(ki), 0),
                   i,
                   j);
-    }
-}
-
-void reduceCTF(Image& dst,
-               const Image& src,
-               const Image& ctf)
-{
-    FOR_EACH_PIXEL_FT(src)
-    {
-        double v = REAL(ctf.iGetFT(i));
-
-        dst[i] = v * src.iGetFT(i) / (gsl_pow_2(v) + CTF_TAU);
-    }
-}
-
-void reduceCTF(Image& dst,
-               const Image& src,
-               const Image& ctf,
-               const int r)
-{
-    IMAGE_FOR_PIXEL_R_FT(r + 1)
-        if (QUAD(i, j) < r * r)
-        {
-            double v = REAL(ctf.getFT(i, j));
-
-            dst.setFT(v * src.getFT(i, j) / (gsl_pow_2(v) + CTF_TAU), i, j);
-        }
-}
-
-void reduceCTF(Image& dst,
-               const Image& src,
-               const Image& ctf,
-               const vec& sigma,
-               const vec& tau,
-               const int pf,
-               const int snrR,
-               const int r)
-{
-    IMAGE_FOR_PIXEL_R_FT(r + 1)
-    {
-        int u = AROUND(NORM(i, j));
-
-        if (u < r)
-        {
-            double v = REAL(ctf.getFT(i, j));
-
-            if (u < snrR)
-                dst.setFT(v * src.getFT(i, j)
-                        / (gsl_pow_2(v) + sigma(u) / tau(pf * u)),
-                          i,
-                          j);
-            else
-                dst.setFT(v * src.getFT(i, j) / (gsl_pow_2(v) + CTF_TAU), i, j);
-        }
     }
 }
