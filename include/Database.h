e750bea9c425b06f4f2bc7535b4efbd2ea9adc5b
Mingxu Hu <humingxu@gmail.com>
2017-03-18 21:42:58 +0800

    Some Thing of Database Added

--- include/Database.h
+++ include/Database.h
@@ -9,81 +9,24 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-#include <cstdio>
-#include <string>
-#include <cstring>
-#include <sys/stat.h>
-
-#include <cerrno>
-#include <cstdio>
-
-#include "Random.h"
 #include "Parallel.h"
-#include "SQLWrapper.h"
 #include "Utils.h"
-
-#define PARTICLE_MODE 0
-#define MICROGRAPH_MODE 1
-
-#define DB_ID_LENGTH 20
-
-#define MAX_LENGTH (1024 * 1024 * 128)
-
-#define SQLITE3_CALLBACK [](void* data, int ncols, char** values, char** header)
-
-#define MASTER_TMP_FILE(database, rank) snprintf((database), sizeof(database), "%s/%s/m/%04d.db", getTempDirectory(), _ID, (rank))
-
-#define SLAVE_TMP_FILE(database) snprintf(database, sizeof(database), "%s/%s/s/%04d.db", getTempDirectory(), _ID, _commRank)
-
-inline void WRITE_FILE(const char* filename, void* buf, size_t len)
-{
-    FILE* fd = fopen(filename, "w");
-    if (fd == NULL)
-        CLOG(FATAL, "LOGGER_SYS") << "Can Not Open Sqlite3 File : "  << filename << strerror(errno);
-    if (fwrite(buf, len, 1, fd) != 1)
-        CLOG(FATAL, "LOGGER_SYS") << "Write " << filename << " failed: " << strerror(errno);
-    fclose(fd);
-}
-
-inline int READ_FILE(const char* filename, void* buf)
-{
-    FILE* fd = fopen(filename, "r");
-    if (fd == NULL)
-        CLOG(FATAL, "LOGGER_SYS") << "Can Not Open Sqlite3 File : "  << filename << strerror(errno);
-    int size = fread(buf, 1, MAX_LENGTH, fd);
-    if (size <= 0)
-        CLOG(FATAL, "LOGGER_SYS") << "Read " << filename << " failed: " << strerror(errno);
-    if (size >= MAX_LENGTH)
-        CLOG(FATAL, "LOGGER_SYS") << "Sqlite3 file " << filename << " exceeds maximum length " << MAX_LENGTH;
-    fclose(fd);
-    return size;
- }
-
-
-
-enum Table
-{
-    Groups,
-    Micrographs,
-    Particles
-};
 
 class Database : public Parallel
 {
     private:
 
-        char _ID[DB_ID_LENGTH + 1];
+        FILE* _db;
 
-        int _mode;
-
-        sql::Statement _stmtAppendGroup;
-        sql::Statement _stmtAppendMicrograph;
-        sql::Statement _stmtAppendParticleS;
-        sql::Statement _stmtAppendParticleL;
-
-    protected:
-
-        sql::DB _db;
+        /**
+         * the ID of the first particle of this process
+         */
+        int _start;
+        
+        /**
+         * the ID of the last particle of this process
+         */
+        int _end;
 
     public:
 
@@ -93,96 +36,45 @@ class Database : public Parallel
 
         ~Database();
 
-        sql::DB& expose() { return _db; }
-
-        void bcastID();
-        /* generate and broadcast an unique ID */
-
-        int mode() const;
-
-        void setMode(const int mode);
-
+        /**
+         * open a .thu file
+         */
         void openDatabase(const char database[]);
 
-        void saveDatabase(sql::DB database);
-
+        /**
+         * save a .thu file
+         */
         void saveDatabase(const char database[]);
 
-        void saveDatabase(const int rank);
+        int start() const { return _start; };
 
-        void createTables();
+        int end() const { return _end; };
 
-        void createTableGroups();
-
-        void createTableMicrographs();
-        
-        void createTableParticles();
-
-        void appendGroup(const char name[],
-                         const int id = -1);
-
-        void appendMicrograph(const char name[],
-                              const double voltage,
-                              const double defocusU,
-                              const double defocusV,
-                              const double defocusAngle,
-                              const double CS,
-                              const int id = -1);
-
-        void appendParticle(const char name[],
-                            const int groupID,
-                            const int micrographID);
-
-        void appendParticle(const char name[],
-                            const int groupId,
-                            const int micrographId,
-                            const double voltage,
-                            const double defocusU,
-                            const double defocusV,
-                            const double defocusAngle,
-                            const double Cs);
-        
+        /**
+         * total number of particles
+         */
         int nParticle();
-        /* number of particles */
 
-        int nMicrograph();
-        /* number of micrographs */
-
+        /**
+         * total number of groups
+         */
         int nGroup();
 
-        void update(const char database[],
-                    const Table table);
+        /**
+         * number of particles assigned to this process
+         */
+        int nParticleRank();
 
-        void prepareTmpFile();
+        /**
+         * assign particles to each process
+         */
+        void assign();
 
-        void gather();
-
-        void scatter();
-
-    protected:
+    private:
 
         void split(int& start,
                    int& end,
                    int commRank);
-
-    private:
-        
-        void setTempInMemory();
-        /* store temp in memory */
-
-        void finalizeStatement();
-
-        void masterPrepareTmpFile();
-
-        void slavePrepareTmpFile();
-
-        void masterReceive(const int rank);
-
-        void slaveReceive();
-
-        void masterSend(const int rank);
-
-        void slaveSend();
 };
 
 #endif // DATABASE_H
--- src/Database.cpp
+++ src/Database.cpp
@@ -8,398 +8,65 @@
 
 #include "Database.h"
 
-Database::Database() : _mode(PARTICLE_MODE)
+Database::Database()
 {
-    _db = sql::DB(":memory:", 0);
-
-    setTempInMemory();
+    _db = NULL;
 }
 
-Database::Database(const char database[]): _mode(PARTICLE_MODE)
+Database::Database(const char database[])
 {
     openDatabase(database);
 }
 
-Database::~Database() {}
-
-void Database::bcastID()
+Database::~Database()
 {
-    gsl_rng* engine = get_random_engine();
-    MLOG(INFO, "LOGGER_INIT") << "Generating an Unique ID of Database";
-    IF_MASTER
-    {
-        for (int i = 0; i < DB_ID_LENGTH; i++)
-            _ID[i] = (char)(gsl_rng_uniform_int(engine, 26) + 65);
-        _ID[DB_ID_LENGTH] = '\0';
-    }
-    MLOG(INFO, "LOGGER_INIT") << "ID is " << _ID;
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    MLOG(INFO, "LOGGER_INIT") << "Broadcasting the Unique ID of Database";
-    MPI_Bcast(_ID, DB_ID_LENGTH + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-}
-
-int Database::mode() const
-{
-    return _mode;
-}
-
-void Database::setMode(const int mode)
-{
-    _mode = mode;
+    fclose(_db);
 }
 
 void Database::openDatabase(const char database[])
 {
-    _db = sql::DB(database, 0);
+    _db = fopen(database, "r");
 
-    setTempInMemory();
-}
-
-void Database::saveDatabase(sql::DB database)
-{
-    sqlite3_backup* backupDB = sqlite3_backup_init(database.getNativeHandle(),
-                                                   "main",
-                                                   _db.getNativeHandle(),
-                                                   "main");
-
-    if (backupDB)
-    {
-        int rc = sqlite3_backup_step(backupDB, -1);
-        if (rc != SQLITE_OK && rc != SQLITE_DONE)
-        {
-            sqlite3_backup_finish(backupDB);
-            throw sql::Exception(rc);
-        }
-        rc = sqlite3_backup_finish(backupDB);
-        if (rc != SQLITE_OK && rc != SQLITE_DONE)
-            throw sql::Exception(rc);
-    }
+    if (_db == NULL) REPORT_ERROR("FAIL TO OPEN DATABASE");
 }
 
 void Database::saveDatabase(const char database[])
 {
-    sql::DB dstDB(database, 0);
-
-    // save to dst database
-    saveDatabase(dstDB);
-}
-
-void Database::saveDatabase(const int rank)
-{
-    char database[64];
-    MASTER_TMP_FILE(database, rank);
-
-    // get the IDs of process commRank
-    int start, end;
-    split(start, end, rank);
-
-    // CLOG(INFO, "LOGGER_SYS") << "Start ID: " << start;
-    // CLOG(INFO, "LOGGER_SYS") << "End ID: " << end;
-
-    vector<const char*> sqls;
-
-    if (_mode == PARTICLE_MODE) {
-        sqls.push_back("insert into dst.groups select distinct groups.* from \
-                  groups, particles where \
-                  (particles.groupID = groups.ID) and \
-                  (particles.ID >= ?1) and (particles.ID <= ?2);");
-        sqls.push_back("insert into dst.micrographs \
-                  select distinct micrographs.* from \
-                  micrographs, particles where \
-                  particles.micrographID = micrographs.ID and \
-                  (particles.ID >= ?1) and (particles.ID <= ?2);");
-        sqls.push_back("insert into dst.particles select * from particles \
-                  where (ID >= ?1) and (ID <= ?2);");
-    }
-    else if (_mode == MICROGRAPH_MODE)
-    {
-        sqls.push_back("insert into dst.micrographs select * from micrographs \
-                  where (ID >= ?1) and (ID <= ?2);");
-        sqls.push_back("insert into dst.particles select particles.* from \
-                  micrographs, particles where \
-                  particles.micrographID = micrographs.ID and \
-                  (micrographs.ID >= ?1) and (micrographs.ID <= ?2);");
-    }
-
-    _db.exec(("attach database '" + string(database) + "' as dst;").c_str());
-
-    try
-    {
-        _db.beginTransaction();
-
-        for (size_t i = 0; i < sqls.size(); ++i)
-        {
-            sql::Statement stmt(sqls[i], -1, _db);
-            stmt.bind_int(1, start);
-            stmt.bind_int(2, end);
-            stmt.step();
-        }
-
-        _db.endTransaction();
-    }
-    catch (...)
-    {
-        _db.rollbackTransaction();
-
-        _db.exec("detach database dst;");
-
-        CLOG(FATAL, "LOGGER_SYS") << "Unable to Save Databases for Each Process";
-    }
-
-    _db.exec("detach database dst;");
-}
-
-void Database::createTables()
-{
-    createTableGroups();
-    createTableMicrographs();
-    createTableParticles();
-}
-
-void Database::createTableGroups()
-{
-    _db.exec("create table groups(ID integer primary key, Name text);");
-
-    const char* sql = "insert into groups values (?, ?);";
-    _stmtAppendGroup = sql::Statement(sql, strlen(sql), _db);
-}
-
-void Database::createTableMicrographs()
-{
-    _db.exec("create table micrographs(ID integer primary key, \
-                                                   Name text, \
-                                                   Voltage real not null, \
-                                                   DefocusU real not null, \
-                                                   DefocusV real not null, \
-                                                   DefocusAngle real not null, \
-                                                   CS real not null);");
-
-    const char* sql = "insert into micrographs \
-                        values (?, ?, ?, ?, ?, ?, ?)";
-    _stmtAppendMicrograph = sql::Statement(sql, strlen(sql), _db);
-}
-
-void Database::createTableParticles()
-{
-    _db.exec("create table particles( \
-                                  ID integer primary key, \
-                                  Name text, \
-                                  GroupID integer not null, \
-                                  MicrographID integer not null, \
-                                  Voltage real, \
-                                  DefocusU real, \
-                                  DefocusV real, \
-                                  DefocusAngle real, \
-                                  Cs real);");
-
-    const char* sqlS = "insert into particles (Name, GroupID, MicrographID) \
-                        values (?, ?, ?)";
-    _stmtAppendParticleS = sql::Statement(sqlS, strlen(sqlS), _db);
-
-    const char* sqlL = "insert into particles (Name, \
-                                               GroupID, \
-                                               MicrographID, \
-                                               Voltage, \
-                                               DefocusU, \
-                                               DefocusV, \
-                                               DefocusAngle, \
-                                               Cs) \
-                        values (?, ?, ?, ?, ?, ?, ?, ?)";
-
-    _stmtAppendParticleL = sql::Statement(sqlL, strlen(sqlL), _db);
-}
-
-void Database::appendGroup(const char name[],
-                           const int id)
-{
-    _stmtAppendGroup.reset();
-    if (id != -1)
-        _stmtAppendGroup.bind_int(1, id);
-    else
-        _stmtAppendGroup.bind_null(1);
-
-    _stmtAppendGroup.bind_text(2, name, strlen(name), true);
-    _stmtAppendGroup.step();
-    _stmtAppendGroup.reset();
-}
-
-void Database::appendMicrograph(const char name[],
-                                const double voltage,
-                                const double defocusU,
-                                const double defocusV,
-                                const double defocusAngle,
-                                const double CS,
-                                const int id)
-{
-    _stmtAppendMicrograph.reset();
-    if (id != -1)
-        _stmtAppendMicrograph.bind_int(1, id);
-    else
-        _stmtAppendMicrograph.bind_null(1);
-
-    _stmtAppendMicrograph.bind_text(2, name, strlen(name), true);
-
-    _stmtAppendMicrograph.bind_double(3, voltage);
-
-    _stmtAppendMicrograph.bind_double(4, defocusU);
-
-    _stmtAppendMicrograph.bind_double(5, defocusV);
-
-    _stmtAppendMicrograph.bind_double(6, defocusAngle);
-
-    _stmtAppendMicrograph.bind_double(7, CS);
-
-    _stmtAppendMicrograph.step();
-
-    _stmtAppendMicrograph.reset();
-}
-
-void Database::appendParticle(const char name[],
-                              const int groupID,
-                              const int micrographID)
-{
-    _stmtAppendParticleS.reset();
-    _stmtAppendParticleS.bind_text(1, name, strlen(name), true);
-    _stmtAppendParticleS.bind_int(2, groupID);
-    _stmtAppendParticleS.bind_int(3, micrographID);
-    _stmtAppendParticleS.step();
-    _stmtAppendParticleS.reset();
-}
-
-void Database::appendParticle(const char name[],
-                              const int groupID,
-                              const int micrographID,
-                              const double voltage,
-                              const double defocusU,
-                              const double defocusV,
-                              const double defocusAngle,
-                              const double Cs)
-{
-    _stmtAppendParticleL.reset();
-    _stmtAppendParticleL.bind_text(1, name, strlen(name), true);
-    _stmtAppendParticleL.bind_int(2, groupID);
-    _stmtAppendParticleL.bind_int(3, micrographID);
-    _stmtAppendParticleL.bind_double(4, voltage);
-    _stmtAppendParticleL.bind_double(5, defocusU);
-    _stmtAppendParticleL.bind_double(6, defocusV);
-    _stmtAppendParticleL.bind_double(7, defocusAngle);
-    _stmtAppendParticleL.bind_double(8, Cs);
-    _stmtAppendParticleL.step();
-    _stmtAppendParticleL.reset();
+    // TODO
 }
 
 int Database::nParticle()
 {
-    sql::Statement count("select count(*) from particles", -1, _db);
-    if (count.step())
-        return count.get_int(0);
-    return 0;
-}
-
-int Database::nMicrograph()
-{
-    sql::Statement count("select count(*) from micrographs", -1, _db);
-    if (count.step())
-        return count.get_int(0);
-    return 0;
+    // TODO
 }
 
 int Database::nGroup()
 {
-    sql::Statement count("select count(*) from groups", -1, _db);
-    if (count.step())
-        return count.get_int(0);
-    return 0;
+    // TODO
 }
 
-void Database::update(const char database[],
-                      const Table table)
+int Database::nParticleRank()
 {
-        string sql = "attach database '" + string(database) + "' as src";
-        _db.exec(sql.c_str());
-    try
-    {
-        _db.beginTransaction();
-
-        switch (table) {
-        case Groups:
-            _db.exec("replace into particles select * from src.particles");
-            break;
-
-        case Micrographs:
-            _db.exec("replace into micrographs select * from src.micrographs");
-            break;
-
-        case Particles:
-            _db.exec("replace into particles select * from src.particles");
-            break;
-        }
-
-        _db.endTransaction();
-    }
-    catch (...)
-    {
-        _db.rollbackTransaction();
-        _db.exec("detach database src");
-        throw;
-    }
-        _db.exec("detach database src");
+    // TODO
 }
 
-void Database::prepareTmpFile()
+int Database::assign()
 {
-    if (_commRank == 0)
-        masterPrepareTmpFile();
-    else
-        slavePrepareTmpFile();
-}
+    IF_MASTER return;
 
-void Database::gather()
-{
-    if (_commRank == 0)
-        for (int i = 1; i < _commSize; i++)
-            masterReceive(i);
-    else
-        slaveSend();
-}
-
-void Database::scatter()
-{
-    if (_commRank == 0)
-    {
-        CLOG(INFO, "LOGGER_SYS") << "Total Number of Particles: " << nParticle();
-        CLOG(INFO, "LOGGER_SYS") << "Total Number of Micrographs: " << nMicrograph();
-        CLOG(INFO, "LOGGER_SYS") << "Total Number of Groups: " << nGroup();
-
-        for (int i = 1; i < _commSize; i++)
-        {
-            saveDatabase(i);
-            masterSend(i);
-        }
-    }
-    else
-        slaveReceive();
+    split(_start, _end, _commRank);
 }
 
 void Database::split(int& start,
                      int& end,
                      const int commRank)
 {
-    int size;
-    if (_mode == PARTICLE_MODE)
-        size = nParticle();
-    else if (_mode == MICROGRAPH_MODE)
-        size = nMicrograph();
-    else
-        abort();
+    int size = nParticle();
 
     int piece = size / (_commSize - 1);
 
-    if (commRank <= size % (_commSize - 1)) {
+    if (commRank <= size % (_commSize - 1))
+    {
         start = (piece + 1) * (commRank - 1) + 1;
         end = start + (piece + 1) - 1;
     }
@@ -408,160 +75,4 @@ void Database::split(int& start,
         start = piece * (commRank - 1) + size % (_commSize - 1) + 1;
         end = start + piece - 1;
     }
-}
-
-void Database::setTempInMemory()
-{
-    _db.exec("pragma temp_store = memory");
-}
-
-void Database::finalizeStatement()
-{
-    // no-op
-}
-
-static void executeCommand(const char* cmd)
-{
-    int rc = system(cmd);
-    if (rc != 0)
-        CLOG(FATAL, "LOGGER_SYS") << "Failure executing command \"" << cmd << "\" with exit status " << rc;
-}
-
-void Database::masterPrepareTmpFile()
-{
-    if (_commRank != 0)
-        return;
-
-    // open dst database
-
-    char die[256];
-    char cast[256];
-    char cmd[1024];
-
-    snprintf(cmd, sizeof(cmd), "mkdir -p  %s/%s/m %s/%s/s", getTempDirectory(), _ID, 
-            getTempDirectory(), _ID);
-    executeCommand(cmd);
-
-    MASTER_TMP_FILE(die, 0);
-
-    string sql;
-    // create a database struct for each node
-    sql = "attach database '" + string(die) + "' as dst";
-    _db.exec(sql.c_str());
-    try
-    {
-        _db.beginTransaction();
-
-        _db.exec("create table dst.groups as select * from groups where 1=0");
-
-        _db.exec("create table dst.micrographs as select * from micrographs where 1=0");
-
-        _db.exec("create table dst.particles as select * from particles where 1=0");
-
-        _db.endTransaction();
-    }
-    catch (...)
-    {
-        _db.rollbackTransaction();
-        _db.exec("detach database dst");
-
-        throw;
-    }
-        _db.exec("detach database dst");
-
-
-    for (int i = 1; i < _commSize; i++) {
-        MASTER_TMP_FILE(cast, i);
-        snprintf(cmd, sizeof(cmd), "cp %s %s", die, cast);
-        executeCommand(cmd);
-    }
-}
-
-void Database::slavePrepareTmpFile()
-{
-    if (_commRank == 0) return;
-
-    char cmd[512];
-    snprintf(cmd, sizeof(cmd), "mkdir -p %s/%s/s", getTempDirectory(), _ID);
-    executeCommand(cmd);
-}
-
-void Database::masterReceive(const int rank)
-{
-    if (_commRank != 0)
-        return;
-
-    MPI_Status status;
-
-    char database[128];
-    MASTER_TMP_FILE(database, rank);
-
-    char* buf = new char[MAX_LENGTH];
-
-    MPI_Recv(buf, MAX_LENGTH, MPI_BYTE, rank, 0, MPI_COMM_WORLD, &status);
-
-    int len;
-    MPI_Get_count(&status, MPI_BYTE, &len);
-
-    WRITE_FILE(database, buf, len);
-
-    delete[] buf;
-
-    update(database, Particles);
-}
-
-void Database::slaveReceive()
-{
-    if (_commRank == 0)
-        return;
-
-    MPI_Status status;
-
-    char database[128];
-    SLAVE_TMP_FILE(database);
-
-    char* buf = new char[MAX_LENGTH];
-
-    MPI_Recv(buf, MAX_LENGTH, MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
-
-    int len;
-    MPI_Get_count(&status, MPI_BYTE, &len);
-
-    WRITE_FILE(database, buf, len);
-
-    delete[] buf;
-
-    openDatabase(database);
-}
-
-void Database::masterSend(const int rank)
-{
-    if (_commRank != 0)
-        return;
-
-    char database[128];
-    MASTER_TMP_FILE(database, rank);
-
-    char* buf = new char[MAX_LENGTH];
-
-    int len = READ_FILE(database, buf);
-    MPI_Ssend(buf, len, MPI_BYTE, rank, 0, MPI_COMM_WORLD);
-
-    delete[] buf;
-}
-
-void Database::slaveSend()
-{
-    if (_commRank == 0)
-        return;
-
-    char database[128];
-    SLAVE_TMP_FILE(database);
-
-    char* buf = new char[MAX_LENGTH];
-
-    int len = READ_FILE(database, buf);
-    MPI_Ssend(buf, len, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
-
-    delete[] buf;
 }
