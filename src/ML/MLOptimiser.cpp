89b6056f4223a8e0f23fda68f29cdf80a7e4454f
Mingxu Hu <humingxu@gmail.com>
2017-03-29 11:46:29 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -1299,9 +1299,9 @@ void MLOptimiser::run()
     {
         _model.resetReco();
 
-        _model.reco(0).setMAP(false);
+        //_model.reco(0).setMAP(false);
 
-        //_model.reco(0).setJoinHalf(true);
+        _model.reco(0).setJoinHalf(true);
     }
 
     MLOG(INFO, "LOGGER_ROUND") << "Reconstructing References(s) at Nyquist";
