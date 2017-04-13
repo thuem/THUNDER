a6966f99707baabdb9c57977ec8b041c677a0bbe
Mingxu Hu <humingxu@gmail.com>
2017-04-13 14:07:39 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -1282,8 +1282,10 @@ void MLOptimiser::run()
         MLOG(INFO, "LOGGER_ROUND") << "Updating Frequency Boundary of Reconstructor";
         _model.updateRU();
 
+        /***
         MLOG(INFO, "LOGGER_ROUND") << "Solvent Flattening";
         solventFlatten(_para.performMask);
+        ***/
 
         NT_MASTER
         {
