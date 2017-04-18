3b68d061b11724b64cda50dd686615b278c76ade
Mingxu Hu <humingxu@gmail.com>
2017-04-18 20:46:03 +0800

    More

--- appsrc/thunder.cpp
+++ appsrc/thunder.cpp
@@ -121,6 +121,11 @@ int main(int argc, char* argv[])
 
     MPI_Init(&argc, &argv);
 
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    CLOG(INFO, "LOGGER_SYS") << "Process " << rank << "Initialised";
+
     CLOG(INFO, "LOGGER_SYS") << "Initialising Threads Setting in FFTW";
 
     fftw_init_threads();
