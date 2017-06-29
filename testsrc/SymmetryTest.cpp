715b04c47c1449339916458d9b73e6ae5b63535a
Mingxu Hu <humingxu@gmail.com>
2017-06-29 16:06:01 +0800

    More

--- testsrc/SymmetryTest.cpp
+++ testsrc/SymmetryTest.cpp
@@ -17,13 +17,13 @@
 
 //#define TEST_SAME_MATRIX
 
-//#define TEST_COUNTERPART
+#define TEST_COUNTERPART
 
 //#define ANCHOR_POINT
 
-#define TEST
+//#define TEST
 
-#define SYM "D2"
+#define SYM "D1"
 
 #define N 1000
 
