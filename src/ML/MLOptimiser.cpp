837128a305d774ca89332da16ed080e666fc7e94
Mingxu Hu <humingxu@gmail.com>
2017-07-02 18:22:55 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -930,13 +930,15 @@ void MLOptimiser::expectation()
             }
 #endif
 
-            //_par[l].calVari();
+            _par[l].calVari();
+
+            _par[l].resample();
+            /***
 
             _par[l].flatten(FLATTEN_THRESHOLD);
 
-            //_par[l].resample();
-
             _par[l].calVari();
+            ***/
 
             if (phase >= ((_searchType == SEARCH_TYPE_GLOBAL)
                         ? MIN_N_PHASE_PER_ITER_GLOBAL
