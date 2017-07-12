99803d02c701d0a10984b7f59c5e13e88e7dd1e0
Mingxu Hu <humingxu@gmail.com>
2017-07-12 20:38:14 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -702,8 +702,8 @@ void MLOptimiser::expectation()
                     v(i) = -GSL_DBL_MAX;
 #endif
 
-            PROCESS_LOGW_SOFT(v);
-            //PROCESS_LOGW_HARD(v);
+            //PROCESS_LOGW_SOFT(v);
+            PROCESS_LOGW_HARD(v);
 
             topW.col(l) = v;
         }
@@ -747,12 +747,13 @@ void MLOptimiser::expectation()
             }
 #endif
 
-            /***
             _par[l].flatten(FLATTEN_THRESHOLD);
+
+            /***
             _par[l].segment(FLATTEN_THRESHOLD);
-            ***/
 
             _par[l].resample();
+            ***/
 
             _par[l].calVari();
 
