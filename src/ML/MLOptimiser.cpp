e8ae2ca257624fa9ed43ae26713367d09617a599
Mingxu Hu <humingxu@gmail.com>
2017-07-22 11:31:51 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -1053,13 +1053,12 @@ void MLOptimiser::expectation()
             }
 #endif
 
-            /***
             _par[l].calVari();
 
             _par[l].resample(_para.mLR, PAR_R);
             _par[l].resample(_para.mLT, PAR_T);
-            ***/
 
+            /***
             double k1 = _par[l].k1();
             double s0 = _par[l].s0();
             double s1 = _par[l].s1();
@@ -1078,6 +1077,7 @@ void MLOptimiser::expectation()
             _par[l].setS0(GSL_MAX_DBL(MIN_STD_FACTOR * s0 / sqrt(_par[l].nT()), _par[l].s0()));
 
             _par[l].setS1(GSL_MAX_DBL(MIN_STD_FACTOR * s1 / sqrt(_par[l].nT()), _par[l].s1()));
+            ***/
 
             if (phase >= ((_searchType == SEARCH_TYPE_GLOBAL)
                         ? MIN_N_PHASE_PER_ITER_GLOBAL
