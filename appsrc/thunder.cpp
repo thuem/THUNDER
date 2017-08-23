38a5e34b3331174e2e19d18776978ef21c5aa0e4
Mingxu Hu <humingxu@gmail.com>
2017-08-23 16:37:30 +0800

    More

--- appsrc/thunder.cpp
+++ appsrc/thunder.cpp
@@ -125,11 +125,11 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    loggerInit(argc, argv);
+
     CLOG(INFO, "LOGGER_SYS") << "Initialising Processes";
 
     MPI_Init(&argc, &argv);
-
-    loggerInit(argc, argv);
 
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
