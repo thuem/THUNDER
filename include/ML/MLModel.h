ad895bb5698af6124ab5520fc6f58711fb5145dc
Mingxu Hu <humingxu@gmail.com>
2017-10-24 17:13:51 +0800

    More

--- include/ML/MLModel.h
+++ include/ML/MLModel.h
@@ -83,7 +83,7 @@
  * resolution resolution for averaging reference(s) from A hemisphere and B
  * hemisphere
  */
-#define A_B_AVERAGE_THRES 10
+#define A_B_AVERAGE_THRES 40
 
 #define CUTOFF_BEYOND_RES 0
 
