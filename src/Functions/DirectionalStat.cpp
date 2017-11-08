9438e219bfd2bc4ace49e8f7aabcfa2e3c7379c0
Mingxu Hu <humingxu@gmail.com>
2017-11-08 16:17:37 +0800

    More

--- src/Functions/DirectionalStat.cpp
+++ src/Functions/DirectionalStat.cpp
@@ -127,8 +127,8 @@ void inferACG(mat44& dst,
 
 #ifndef NAN_NO_CHECK
 
-    for (int i = 0; i < dst.nrows(); i++)
-        for (int j = 0; j < dst.ncols(); j++)
+    for (int i = 0; i < dst.rows(); i++)
+        for (int j = 0; j < dst.cols(); j++)
             if (gsl_isnan(dst(i, j)))
             {
                 REPORT_ERROR("NAN DETECTED");
