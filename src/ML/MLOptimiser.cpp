b8a4377364c88e81b9b0c95f294ef1bd795eca28
Mingxu Hu <humingxu@gmail.com>
2017-11-21 21:36:12 +0800

    More

--- src/ML/MLOptimiser.cpp
+++ src/ML/MLOptimiser.cpp
@@ -4092,7 +4092,7 @@ void MLOptimiser::reconstructRef(const bool fscFlag,
 
         MPI_Barrier(MPI_COMM_WORLD);
 
-        //_model.compareTwoHemispheres(false, true, _para.thresReportFSC);
+        _model.compareTwoHemispheres(false, false, _para.thresReportFSC);
     }
 
     ALOG(INFO, "LOGGER_ROUND") << "Freeing Space for Pre-calcuation in Reconstruction";
