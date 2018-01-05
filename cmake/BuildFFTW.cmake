42cd2310700f34bac73409a6516aa188fa1bd659
Mingxu Hu <humingxu@gmail.com>
2018-01-05 21:57:53 +0800

    Have a Try

---
+++ cmake/BuildFFTW.cmake
@@ -0,0 +1,30 @@
+set(FFTW_PATH "${PROJECT_SOURCE_DIR}/external/fftw")
+
+if(SINGLE_PRECISION)
+    set(libfft "thunder_fftw3f")
+    set(ext_conf_flags_fft --enable-shared --enable-float --prefix=${FFTW_EXTERNAL_PATH})
+    if(TARGET_X86)
+        set(ext_conf_flags_fft ${ext_conf_flags_fft} --enable-sse --enable-avx)
+    endif(TARGET_X86)
+else(SINGLE_PRECISION)
+    set(libfft "thunder_fftw3")
+    set(ext_conf_flags_fft --enable-shared --prefix=${FFTW_EXTERNAL_PATH})
+    if(TARGET_X86)
+        set(ext_conf_flags_fft ${ext_conf_flags_fft} --enable-sse2 --enable-avx)
+    endif(TARGET_X86)
+endif(SINGLE_PRECISION)
+
+set(FFTW_LIB "${FFTW_EXTERNAL_PATH}/lib/${libfft}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+set(FFTW_INCLUDE "${FFTW_EXTERNAL_PATH}/include")
+
+externalproject_add(FFTW
+                    SOURCE_DIR ${PROJECT_SOURCE_DIR}/external/packages/fftw-3.3.7
+                    CONFIGURE_COMMAND <SOURCE_DIR>/configure ${ext_conf_flags_fft}
+                    INSTALL_DIR ${FFTW_EXTERNAL_PATH}/fftw3
+                    BINARY_DIR ${FFTW_EXTERNAL_PATH}/fftw3
+                    LOG_INSTALL)
+
+                #set(BUILD_OWN_FFTW TRUE)
+
+                #message(STATUS "FFTW_INCLUDES:     ${FFTW_INCLUDES}")
+                #message(STATUS "FFTW_LIBRARIES:    ${FFTW_LIBRARIES}")
