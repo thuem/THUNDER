090c691f82ae332cec7b066f360fa52edd15a364
Mingxu Hu <humingxu@gmail.com>
2018-01-16 18:34:06 +0800

    More

--- include/Model.h
+++ include/Model.h
@@ -87,7 +87,7 @@
  * resolution resolution for averaging reference(s) from A hemisphere and B
  * hemisphere
  */
-#define A_B_AVERAGE_THRES 40
+#define A_B_AVERAGE_THRES 15
 
 #define CUTOFF_BEYOND_RES 0
 
