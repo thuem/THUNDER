26a641d528aeeb9d8f2b6996a5ebf25ce7f92676
Mingxu Hu <humingxu@gmail.com>
2018-01-28 14:22:43 +0800

    More

--- cmake/BuildFFTW.cmake
+++ cmake/BuildFFTW.cmake
@@ -3,15 +3,22 @@ set(FFTW_PATH "${PROJECT_BINARY_DIR}/external/fftw")
 if(SINGLE_PRECISION)
     set(FFTW_LIBRARIES ${FFTW_PATH}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}fftw3f${CMAKE_STATIC_LIBRARY_SUFFIX})
     set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTW_PATH}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}fftw3f_threads${CMAKE_STATIC_LIBRARY_SUFFIX})
-    #set(ext_conf_flags_fft --enable-threads --enable-shared --enable-float --prefix=${FFTW_PATH})
     set(ext_conf_flags_fft --enable-threads --enable-float --prefix=${FFTW_PATH})
-    #set(ext_conf_flags_fft ${ext_conf_flags_fft} --enable-sse --enable-avx)
 else(SINGLE_PRECISION)
     set(FFTW_LIBRARIES ${FFTW_PATH}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}fftw3${CMAKE_STATIC_LIBRARY_SUFFIX})
     set(FFTW_LIBRARIES ${FFTW_LIBRARIES} ${FFTW_PATH}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}fftw3_threads${CMAKE_STATIC_LIBRARY_SUFFIX})
     set(ext_conf_flags_fft --enable-threads --prefix=${FFTW_PATH})
-    #set(ext_conf_flags_fft ${ext_conf_flags_fft} --enable-sse --enable-avx)
 endif(SINGLE_PRECISION)
+
+if(ENABLE_SIMD_512)
+    set(ext_conf_flags_fft "${ext_conf_flags_fft} --enable-avx512")
+endif(ENABLE_SIMD_512)
+
+if(ENABLE_SIMD_256)
+    set(ext_conf_flags_fft "${ext_conf_flags_fft} --enable-avx --enable-avx2")
+endif(ENABLE_SIMD_256)
+
+message(STATUS "FFTW_FLAGS : ${ext_conf_flags_fft}")
 
 message(STATUS "FFTW_LIBRARIES : ${FFTW_LIBRARIES}")
 
