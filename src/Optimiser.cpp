cfd25f11438e14df16f92418ae5e4f42c8162464
Mingxu Hu <humingxu@gmail.com>
2018-01-29 16:07:59 +0800

    More

--- src/Optimiser.cpp
+++ src/Optimiser.cpp
@@ -312,8 +312,8 @@ void Optimiser::init()
 
     _r = AROUND(resA2P(1.0 / _para.initRes, _para.size, _para.pixelSize)) + 1;
     _model.setR(_r);
-    _model.setRPrev(_r);
-    _model.setRT(_r);
+    //_model.setRPrev(_r);
+    //_model.setRT(_r);
 
     MLOG(INFO, "LOGGER_INIT") << "Setting MPI Environment of _exp";
     _db.setMPIEnv(_commSize, _commRank, _hemi);
