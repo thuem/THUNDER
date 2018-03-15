26de6b4be2adebe03cadf8ed2abd960763692e42
Mingxu Hu <humingxu@gmail.com>
2018-03-15 20:47:26 +0800

    More

--- include/Optimiser.h
+++ include/Optimiser.h
@@ -62,9 +62,9 @@
 
 #define MIN_STD_FACTOR 3
 
-#define CLASS_BALANCE_FACTOR 0.05
+#define CLASS_BALANCE_FACTOR 0.01
 
-#define MIN_N_IMAGES_PER_CLASS 30
+#define MIN_N_IMAGES_PER_CLASS 5
 
 struct OptimiserPara
 {
