60fb65e95c292cce6da26294cab930c32060c6ad
Mingxu Hu <humingxu@gmail.com>
2018-03-30 15:10:47 +0800

    More

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -1016,7 +1016,7 @@ void Particle::calVari(const ParticleType pt)
             }
             ***/
 
-            symmetrise(&_topR);
+            // symmetrise(&_topR);
 
 #ifdef PARTICLE_ROT_MEAN_USING_STAT_CAL_VARI
 
