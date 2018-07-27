6aa16a004cb9470ecb631715cd77298a58d97a7c
Mingxu Hu <humingxu@gmail.com>
2018-07-27 20:01:05 +0800

    More

--- src/Reconstructor.cpp
+++ src/Reconstructor.cpp
@@ -1411,7 +1411,7 @@ void Reconstructor::reconstruct(Volume& dst)
                 SEGMENT_NAN_CHECK_COMPLEX(_C2D.dataFT(), _C2D.sizeFT());
 #endif
 
-                #pragma omp parallel for schedule(dynamic)
+                //#pragma omp parallel for schedule(dynamic)
                 IMAGE_FOR_EACH_PIXEL_FT(_W2D)
                     if (QUAD(i, j) < gsl_pow_2(_maxRadius * _pf))
                     {
@@ -1431,7 +1431,6 @@ void Reconstructor::reconstruct(Volume& dst)
                                        i,
                                        j);
 
-                        /***
                         if (IS_NAN(REAL(_W2D.getFTHalf(i, j)))
                          || IS_NAN(IMAG(_W2D.getFTHalf(i, j))))
                         {
@@ -1442,7 +1441,6 @@ void Reconstructor::reconstruct(Volume& dst)
 
                             abort();
                         }
-                        ***/
                     }
 
 #ifndef NAN_NO_CHECK
