99b8aaaf8c2fe12b2e89672451b4989616a4d1bc
Mingxu Hu <humingxu@gmail.com>
2018-08-02 17:54:46 +0800

    more

--- src/Optimiser.cpp
+++ src/Optimiser.cpp
@@ -7744,6 +7744,7 @@ void Optimiser::solventFlatten(const bool mask)
             }
             else if (_para.mode == MODE_3D)
             {
+                /***
 #ifdef OPTIMISER_SOLVENT_FLATTEN_LOW_PASS_MASK
 
                 fft.fwMT(_mask);
@@ -7773,6 +7774,7 @@ void Optimiser::solventFlatten(const bool mask)
 #endif
 
 #endif
+                ***/
             }
             else
             {
