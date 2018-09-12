9f7964ce67069cd9a438c9f6a62c64200cd65595
Huabin Ruan <ruanhuabin@tsinghua.edu.cn>
2018-09-12 18:34:57 +0800

    1. Add comments for some functions in Complex.h 2. Delte some never used code.

--- include/Complex.h
+++ include/Complex.h
@@ -21,10 +21,8 @@
 inline Complex COMPLEX_POLAR(const RFLOAT phi /**< [in] Angle value @f$\phi@f$ */)
 {
     Complex z;
-
     z.dat[0] = TS_COS(phi);
     z.dat[1] = TS_SIN(phi);
-
     return z;
 }
 
@@ -33,7 +31,7 @@ inline Complex COMPLEX_POLAR(const RFLOAT phi /**< [in] Angle value @f$\phi@f$ *
 /**
  * @brief Get the conjugate result based on value a
  *
- * @return Conjugate of a. 
+ * @return Conjugate of a.
  */
 inline Complex CONJUGATE(const Complex &a /**< [in] Complex number whose conjuate value needs to be returned */)
 {
@@ -53,7 +51,6 @@ static RFLOAT ts_hypot(const RFLOAT x,
     RFLOAT xabs = fabs(x) ;
     RFLOAT yabs = fabs(y) ;
 #endif
-
     RFLOAT min, max;
 
     if (xabs < yabs)
@@ -61,19 +58,19 @@ static RFLOAT ts_hypot(const RFLOAT x,
         min = xabs;
         max = yabs;
     }
+
     else
     {
         min = yabs;
         max = xabs;
     }
 
-    if (min == 0) 
+    if (min == 0)
     {
         return max;
     }
 
     RFLOAT u = min / max;
-
     return max * TS_SQRT(1 + u * u);
 }
 
@@ -83,7 +80,7 @@ static RFLOAT ts_hypot(const RFLOAT x,
  *
  *  @return The |a| of complex number a.
  */
-inline RFLOAT ABS(const Complex& a /**< [in]  The number whose |a| needs to be calculated*/)
+inline RFLOAT ABS(const Complex &a /**< [in]  The number whose |a| needs to be calculated*/)
 {
     return ts_hypot(a.dat[0], a.dat[1]);
 }
@@ -92,145 +89,154 @@ inline RFLOAT ABS2(const Complex &a)
 {
     RFLOAT result = a.dat[0] * a.dat[0] + a.dat[1] * a.dat[1];
     return result;
-};
+}
 
-inline Complex COMPLEX(RFLOAT a, RFLOAT b)
+
+/**
+ *  @brief Construct a complex number with a as real part and b as image part.
+ *
+ *  @return A initialized complex number.
+ */
+inline Complex COMPLEX(RFLOAT a, /**< [in] Real part */
+                       RFLOAT b  /**< [in] Image part */
+                      )
 {
     Complex z;
     z.dat[0] = a;
     z.dat[1] = b;
     return z;
-};
+}
 
-inline RFLOAT REAL(const Complex& a)
+
+/**
+ *  @brief Get the real part of the complex number a
+ *
+ *  @return Complex number a's real part
+ */
+inline RFLOAT REAL(const Complex &a /**< [in] Complex number to be operated */)
 {
     return a.dat[0];
-};
+}
 
-inline RFLOAT IMAG(const Complex& a)
+
+/**
+ *  @brief Get the image part of the complex number a
+ *
+ *  @return Complex number a's image part
+ */
+inline RFLOAT IMAG(const Complex &a /**< [in] Complex number to be operated */)
 {
     return a.dat[1];
-};
+}
 
-inline RFLOAT gsl_real(const Complex& a)
-{
-    return a.dat[0];
-};
 
-inline RFLOAT gsl_imag(const Complex& a)
-{
-    return a.dat[1];
-};
-
-inline RFLOAT gsl_real_imag_sum(const Complex& a)
+/**
+ *  @brief Calcuate the sum of the complex number a's real part and image part
+ *
+ *  @return The sum of a's real part and image part
+ */
+inline RFLOAT gsl_real_imag_sum(const Complex &a /**< [in] Complex number to be operated */)
 {
     return a.dat[0] + a.dat[1];
-};
+}
 
-inline Complex operator-(const Complex& a)
+inline Complex operator-(const Complex &a)
 {
     Complex newa;
-
     newa.dat[0] = -a.dat[0];
     newa.dat[1] = -a.dat[1];
-
     return newa;
-};
+}
 
-inline Complex operator+(const Complex& a, const Complex& b)
+inline Complex operator+(const Complex &a, const Complex &b)
 {
     Complex result;
-
     result.dat[0] = a.dat[0] + b.dat[0];
     result.dat[1] = a.dat[1] + b.dat[1];
-
     return result;
-};
+}
 
-inline Complex operator-(const Complex& a, const Complex& b)
+inline Complex operator-(const Complex &a, const Complex &b)
 {
     Complex result;
-
     result.dat[0] = a.dat[0] - b.dat[0];
     result.dat[1] = a.dat[1] - b.dat[1];
-
     return result;
-};
+}
 
-inline Complex operator*(const Complex& a, const Complex& b)
+inline Complex operator*(const Complex &a, const Complex &b)
 {
     Complex result;
-
     result.dat[0] = a.dat[0] * b.dat[0] - a.dat[1] * b.dat[1];
     result.dat[1] = a.dat[0] * b.dat[1] + a.dat[1] * b.dat[0];
-
     return result;
-};
+}
 
-inline Complex operator/(const Complex& a, const Complex& b)
+inline Complex operator/(const Complex &a, const Complex &b)
 {
     Complex result;
-
     result.dat[0] = a.dat[0] * b.dat[0] + a.dat[1] * b.dat[1];
-    result.dat[1] = a.dat[1] * b.dat[0] - a.dat[0] * b.dat[1]; 
-    
+    result.dat[1] = a.dat[1] * b.dat[0] - a.dat[0] * b.dat[1];
     RFLOAT norm = b.dat[0] * b.dat[0] + b.dat[1] * b.dat[1];
-
     result.dat[0] /= norm;
     result.dat[1] /= norm;
-
     return result;
-};
+}
 
-inline void operator+=(Complex& a, const Complex b) { a = a + b; };
+inline void operator+=(Complex &a, const Complex b)
+{
+    a = a + b;
+}
 
-inline void operator-=(Complex& a, const Complex b) { a = a - b; };
+inline void operator-=(Complex &a, const Complex b)
+{
+    a = a - b;
+}
 
-inline void operator*=(Complex& a, const Complex b) { a = a * b; };
+inline void operator*=(Complex &a, const Complex b)
+{
+    a = a * b;
+}
 
-inline void operator/=(Complex& a, const Complex b) { a = a / b; };
+inline void operator/=(Complex &a, const Complex b)
+{
+    a = a / b;
+}
 
 inline Complex operator*(const Complex a, const RFLOAT x)
 {
     Complex result;
-
     result.dat[0] = a.dat[0] * x;
     result.dat[1] = a.dat[1] * x;
-
     return result;
-};
+}
 
 inline Complex operator*(const RFLOAT x, const Complex a)
 {
     Complex result;
-
     result.dat[0] = a.dat[0] * x;
     result.dat[1] = a.dat[1] * x;
-
     return result;
-};
+}
 
-inline void operator*=(Complex& a, const RFLOAT x)
+inline void operator*=(Complex &a, const RFLOAT x)
 {
     a.dat[0] = a.dat[0] * x;
     a.dat[1] = a.dat[1] * x;
-};
+}
 
 inline Complex operator/(const Complex a, const RFLOAT x)
 {
     Complex result;
-
     result.dat[0] = a.dat[0] / x;
     result.dat[1] = a.dat[1] / x;
-
     return result;
+}
 
-};
-
-inline void operator/=(Complex& a, const RFLOAT x)
+inline void operator/=(Complex &a, const RFLOAT x)
 {
     a.dat[0] = a.dat[0] / x;
     a.dat[1] = a.dat[1] / x;
-};
+}
 
 #endif // COMPLEX_H
