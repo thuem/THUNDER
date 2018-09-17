78d3e100f59d37f7eb158ce8861a0e3911588824
Huabin Ruan <ruanhuabin@tsinghua.edu.cn>
2018-09-17 11:49:17 +0800

    1. Add comments in header of Precision. 2. Delete some unused code. 3. Code can be compiled correctly.

--- include/Precision.h
+++ include/Precision.h
@@ -1,51 +1,19 @@
-/*******************************************************************************
- * Author: Huabin Ruan
- * Dependecy:
- * Test:
- * Execution:
- * Description:
- * ****************************************************************************/
+/** @file
+ *  @author Huabin Ruan
+ *  @version 1.4.11.180913
+ *  @copyright THUNDER Non-Commercial Software License Agreement
+ *
+ *  ChangeLog
+ *  AUTHOR      | TIME       | VERSION       | DESCRIPTION
+ *  ------      | ----       | -------       | -----------
+ *  Huabin Ruan | 2018/09/13 | 1.4.11.080913 | add header for file and functions
+ *
+ *  @brief Precision.h encapsulates the header files/MACRO/data structures/functions of the single-precision version and the double-precision version
+  */
 
 #ifndef  PRECISION_H
 #define  PRECISION_H
 
-/*
- *#define TSGSL_cdf_chisq_Qinv gsl_cdf_chisq_Qinv
- *#define TSGSL_cdf_gaussian_Qinv gsl_cdf_gaussian_Qinv
- *#define TSGSL_complex_abs2 gsl_complex_abs2
- *#define TSGSL_fit_linear gsl_fit_linear
- *#define TSGSL_isinf gsl_isinf
- *#define TSGSL_isnan gsl_isnan
- *#define TSGSL_pow_2 gsl_pow_2
- *#define TSGSL_pow_3 gsl_pow_3
- *#define TSGSL_pow_4 gsl_pow_4
- *#define TSGSL_ran_bivariate_gaussian gsl_ran_bivariate_gaussian
- *#define TSGSL_ran_dir_2d gsl_ran_dir_2d
- *#define TSGSL_ran_flat gsl_ran_flat
- *#define TSGSL_ran_gaussian gsl_ran_gaussian
- *#define TSGSL_ran_shuffle gsl_ran_shuffle
- *#define TSGSL_rng_alloc gsl_rng_alloc
- *#define TSGSL_rng_free gsl_rng_free
- *#define TSGSL_rng_get gsl_rng_get
- *#define TSGSL_rng_set gsl_rng_set
- *#define TSGSL_rng_uniform gsl_rng_uniform
- *#define TSGSL_rng_uniform_int gsl_rng_uniform_int
- *#define TSGSL_sf_bessel_I0 gsl_sf_bessel_I0
- *#define TSGSL_sf_bessel_In gsl_sf_bessel_In
- *#define TSGSL_sf_bessel_Inu gsl_sf_bessel_Inu
- *#define TSGSL_sf_bessel_j0 gsl_sf_bessel_j0
- *#define TSGSL_sf_bessel_Jnu gsl_sf_bessel_Jnu
- *#define TSGSL_sf_sinc gsl_sf_sinc
- *#define TSGSL_sort gsl_sort
- *#define TSGSL_sort_largest gsl_sort_largest
- *#define TSGSL_stats_max gsl_stats_max
- *#define TSGSL_stats_mean gsl_stats_mean
- *#define TSGSL_stats_min gsl_stats_min
- *#define TSGSL_stats_quantile_from_sorted_data gsl_stats_quantile_from_sorted_data
- *#define TSGSL_stats_sd gsl_stats_sd
- *#define TSGSL_stats_sd_m gsl_stats_sd_m
- *
- */
 #include <gsl/gsl_blas.h>
 #include <gsl/gsl_cdf.h>
 #include <gsl/gsl_complex.h>
@@ -62,17 +30,10 @@
 #include "THUNDERConfig.h"
 #include "Config.h"
 
-/*
- *#define ENABLE_SIMD_256 1
- */
-
-/*
- *#define ENABLE_SIMD_512 1
- */
 #ifdef SINGLE_PRECISION
-#include <fftw3.h>
+    #include <fftw3.h>
 #else
-#include <fftw3.h>
+    #include <fftw3.h>
 #endif
 
 
@@ -107,7 +68,7 @@ inline RFLOAT TS_SIN(const RFLOAT x)
 #else
     return sin(x);
 #endif
-};
+}
 
 inline RFLOAT TS_COS(const RFLOAT x)
 {
@@ -116,7 +77,7 @@ inline RFLOAT TS_COS(const RFLOAT x)
 #else
     return cos(x);
 #endif
-};
+}
 
 inline RFLOAT TS_SQRT(const RFLOAT x)
 {
@@ -125,19 +86,19 @@ inline RFLOAT TS_SQRT(const RFLOAT x)
 #else
     return sqrt(x);
 #endif
-};
+}
 
 inline RFLOAT TSGSL_MAX_RFLOAT(const RFLOAT a,
                                const RFLOAT b)
 {
     return a > b ? a : b;
-};
+}
 
 inline RFLOAT TSGSL_MIN_RFLOAT(const RFLOAT a,
                                const RFLOAT b)
 {
     return a < b ? a : b;
-};
+}
 
 RFLOAT TSGSL_cdf_chisq_Qinv (const RFLOAT Q, const RFLOAT nu);
 RFLOAT TSGSL_cdf_gaussian_Qinv (const RFLOAT Q, const RFLOAT sigma);
