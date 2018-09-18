f34d8d3917ec2de2ae68fb6ecafffff2613d6b69
Huabin Ruan <ruanhuabin@tsinghua.edu.cn>
2018-09-18 16:24:50 +0800

    Move arithmetic expression into latex tag @f$

--- include/Complex.h
+++ include/Complex.h
@@ -8,6 +8,7 @@
  *  AUTHOR      | TIME       | VERSION       | DESCRIPTION
  *  ------      | ----       | -------       | -----------
  *  Huabin Ruan | 2018/09/13 | 1.4.11.080913 | add header for file and functions
+ *  Huabin Ruan | 2018/09/18 | 1.4.11.080918 | move arithmetic expression into latex tag 
  *
  *  @brief Complex.h contains functions used for complex number related operations, like +, -, *, /, |c|.
   */
@@ -87,16 +88,21 @@ static RFLOAT ts_hypot(const RFLOAT x,
 
 
 /**
- *  @brief Calculate the |a| of complex number a.
+ *  @brief Calculate the @f$|a|@f$ of complex number @f$a@f$.
  *
- *  @return The |a| of complex number a.
+ *  @return The @f$|a|@f$ of complex number @f$a@f$.
  */
-inline RFLOAT ABS(const Complex &a /**< [in]  The number whose |a| needs to be calculated*/)
+inline RFLOAT ABS(const Complex &a /**< [in] The number whose @f$|a|@f$ needs to be calculated*/)
 {
     return ts_hypot(a.dat[0], a.dat[1]);
 }
 
-inline RFLOAT ABS2(const Complex &a)
+/**
+ *  @brief Calculate the @f$|a|^2@f$ of complex number @f$a@f$.
+ *
+ *  @return The @f$|a|^2@f$ of complex number @f$a@f$.
+ */
+inline RFLOAT ABS2(const Complex &a /**< [in] The number whose @f$|a|^2@f$ needs to be calculated  */)
 {
     RFLOAT result = a.dat[0] * a.dat[0] + a.dat[1] * a.dat[1];
     return result;
@@ -104,7 +110,7 @@ inline RFLOAT ABS2(const Complex &a)
 
 
 /**
- *  @brief Construct a complex number with a as real part and b as image part.
+ *  @brief Construct a complex number with @f$a@f$ as real part and @f$b@f$ as image part.
  *
  *  @return A initialized complex number.
  */
@@ -120,9 +126,9 @@ inline Complex COMPLEX(RFLOAT a, /**< [in] Real part */
 
 
 /**
- *  @brief Get the real part of the complex number a
+ *  @brief Get the real part of the complex number @f$a@f$
  *
- *  @return Complex number a's real part
+ *  @return Complex number @f$a'@f$s real part
  */
 inline RFLOAT REAL(const Complex &a /**< [in] Complex number to be operated */)
 {
@@ -131,9 +137,9 @@ inline RFLOAT REAL(const Complex &a /**< [in] Complex number to be operated */)
 
 
 /**
- *  @brief Get the image part of the complex number a
+ *  @brief Get the image part of the complex number @f$a@f$
  *
- *  @return Complex number a's image part
+ *  @return Complex number @f$a'@f$s image part
  */
 inline RFLOAT IMAG(const Complex &a /**< [in] Complex number to be operated */)
 {
@@ -142,9 +148,9 @@ inline RFLOAT IMAG(const Complex &a /**< [in] Complex number to be operated */)
 
 
 /**
- *  @brief Calcuate the sum of the complex number a's real part and image part
+ *  @brief Calcuate the sum of the complex number @f$a'@f$s real part and image part
  *
- *  @return The sum of a's real part and image part
+ *  @return The sum of @f$a'@f$s real part and image part
  */
 inline RFLOAT gsl_real_imag_sum(const Complex &a /**< [in] Complex number to be operated */)
 {
@@ -152,9 +158,9 @@ inline RFLOAT gsl_real_imag_sum(const Complex &a /**< [in] Complex number to be 
 }
 
 /**
- *  @brief Implement the add operation between two complex numbers, e.g. c = a + b, where a and b are complex numbers.
+ *  @brief Implement the add operation between two complex numbers, e.g. @f$c = a + b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of c = a + b.
+ *  @return the result of @f$c = a + b@f$.
  */
 inline Complex operator+(const Complex &a, /**< [in] First operand used to perform add operation between two complex numbers */
                          const Complex &b  /**< [in] Second operand used to perform add operation between two complex numbers */
@@ -167,9 +173,9 @@ inline Complex operator+(const Complex &a, /**< [in] First operand used to perfo
 }
 
 /**
- *  @brief Implement the sub operation between two complex numbers, e.g. c = a - b, where a and b are complex numbers.
+ *  @brief Implement the sub operation between two complex numbers, e.g. @f$c = a - b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of c = a - b.
+ *  @return the result of @f$c = a - b@f$.
  */
 inline Complex operator-(const Complex &a, /**< [in] First operand used to perform sub operation between two complex numbers */
                          const Complex &b  /**< [in] Second operand used to perform sub operation between two complex numbers */
@@ -182,9 +188,9 @@ inline Complex operator-(const Complex &a, /**< [in] First operand used to perfo
 }
 
 /**
- *  @brief Implement the mul operation between two complex numbers, e.g. c = a * b, where a and b are complex numbers.
+ *  @brief Implement the mul operation between two complex numbers, e.g. @f$c = a * b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of c = a * b.
+ *  @return the result of @f$c = a * b@f$.
  */
 inline Complex operator*(const Complex &a, /**< [in] First operand used to perform sub operation between two complex numbers */
                          const Complex &b  /**< [in] Second operand used to perform sub operation between two complex numbers */
@@ -197,9 +203,9 @@ inline Complex operator*(const Complex &a, /**< [in] First operand used to perfo
 }
 
 /**
- *  @brief Implement the div operation between two complex numbers, e.g. c = a / b, where a and b are complex numbers.
+ *  @brief Implement the div operation between two complex numbers, e.g. @f$c = a / b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of c = a / b.
+ *  @return the result of @f$c = a / b@f$.
  */
 inline Complex operator/(const Complex &a, /**< [in] First operand used to perform div operation between two complex numbers */
                          const Complex &b  /**< [in] Second operand used to perform div operation between two complex numbers */
@@ -215,57 +221,57 @@ inline Complex operator/(const Complex &a, /**< [in] First operand used to perfo
 }
 
 /**
- *  @brief Implement the += operation between two complex numbers, e.g. a += b, where a and b are complex numbers.
+ *  @brief Implement the @f$+=@f$ operation between two complex numbers, e.g. @f$a += b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of a += b.
+ *  @return the result of @f$a += b@f$.
  */
-inline void operator+=(Complex &a,       /**< [in] First operand used to perform += operation between two complex numbers */
-                       const Complex &b /**< [in] Second operand used to perform += operation between two complex numbers */
+inline void operator+=(Complex &a,       /**< [in] First operand used to perform @f$+=@f$ operation between two complex numbers */
+                       const Complex &b /**< [in] Second operand used to perform @f$+=@f$ operation between two complex numbers */
                       )
 {
     a = a + b;
 }
 
 /**
- *  @brief Implement the -= operation between two complex numbers, e.g. a -= b, where a and b are complex numbers.
+ *  @brief Implement the @f$-=@f$ operation between two complex numbers, e.g. @f$a -= b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of a -= b.
+ *  @return the result of @f$a -= b@f$.
  */
-inline void operator-=(Complex &a,       /**< [in] First operand used to perform -= operation between two complex numbers */
-                       const Complex &b /**< [in] Second operand used to perform -= operation between two complex numbers */
+inline void operator-=(Complex &a,       /**< [in] First operand used to perform @f$-=@f$ operation between two complex numbers */
+                       const Complex &b /**< [in] Second operand used to perform @f$-=@f$ operation between two complex numbers */
                       )
 {
     a = a - b;
 }
 
 /**
- *  @brief Implement the *= operation between two complex numbers, e.g. a *= b, where a and b are complex numbers.
+ *  @brief Implement the *= operation between two complex numbers, e.g. @f$a *= b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of a *= b.
+ *  @return the result of @f$a *= b@f$.
  */
-inline void operator*=(Complex &a,       /**< [in] First operand used to perform *= operation between two complex numbers */
-                       const Complex &b /**< [in] Second operand used to perform *= operation between two complex numbers */
+inline void operator*=(Complex &a,       /**< [in] First operand used to perform @f$*=@f$ operation between two complex numbers */
+                       const Complex &b /**< [in] Second operand used to perform @f$*=@f$ operation between two complex numbers */
                       )
 {
     a = a * b;
 }
 
 /**
- *  @brief Implement the /= operation between two complex numbers, e.g. a /= b, where a and b are complex numbers.
+ *  @brief Implement the @f$/=@f$ operation between two complex numbers, e.g. @f$a /= b@f$, where @f$a@f$ and @f$b@f$ are complex numbers.
  *
- *  @return the result of a /= b.
+ *  @return the result of @f$a /= b@f$.
  */
-inline void operator/=(Complex &a,      /**< [in] First operand used to perform /= operation between two complex numbers */
-                       const Complex &b /**< [in] Second operand used to perform /= operation between two complex numbers */
+inline void operator/=(Complex &a,      /**< [in] First operand used to perform @f$/=@f$ operation between two complex numbers */
+                       const Complex &b /**< [in] Second operand used to perform @f$/=@f$ operation between two complex numbers */
                       )
 {
     a = a / b;
 }
 
 /**
- *  @brief Implement the mul operation between a  complex number and a RFLOAT number, e.g. c = a * x, where a is a complex number and x is a RFLOAT number.
+ *  @brief Implement the mul operation between a  complex number and a RFLOAT number, e.g. @f$c = a * x@f$, where @f$a@f$is @f$a@f$ complex number and @f$x@f$ is a RFLOAT number.
  *
- *  @return the result of c = a * x.
+ *  @return the result of @f$c = a * x@f$.
  */
 inline Complex operator*(const Complex a, /**< [in] First operand with type of complex used to perform mul operation.*/
                          const RFLOAT x   /**< [in] Second operand with type of RFLOAT used to perform mul operation. */
@@ -278,9 +284,9 @@ inline Complex operator*(const Complex a, /**< [in] First operand with type of c
 }
 
 /**
- *  @brief Implement the mul operation between a  RFLOAT number and a Complex number, e.g. c = x * a, where x is a RFLOAT number and x is a complex number.
+ *  @brief Implement the mul operation between a  RFLOAT number and a Complex number, e.g. @f$c = x * a@f$, where @f$x @f$is a RFLOAT number and @f$a@f$is a complex number.
  *
- *  @return the result of c = x * a.
+ *  @return the result of @f$c = x * a@f$.
  */
 inline Complex operator*(const RFLOAT x, /**< [in] First operand with type of RFLOAT used to perform mul operation.*/
                          const Complex a /**< [in] Second operand with type of complex used to perform mul operation.*/
@@ -293,12 +299,12 @@ inline Complex operator*(const RFLOAT x, /**< [in] First operand with type of RF
 }
 
 /**
- *  @brief Implement the mul operation between a  complex number and a RFLOAT number, e.g. a *=  x, where a is a complex number and x is a RFLOAT number.
+ *  @brief Implement the mul operation between a  complex number and a RFLOAT number, e.g. @f$a *=  x@f$, where @f$a@f$ is a complex number and @f$x@f$ is a RFLOAT number.
  *
- *  @return the result of a *= x.
+ *  @return the result of @f$a *= x@f$.
  */
-inline void operator*=(Complex& a,    /**< [in] First operand with type of complex used to perform *= operation.*/
-                       const RFLOAT x /**< [in] Second operand with type of RFLOAT used to perform *= operation. */
+inline void operator*=(Complex& a,    /**< [in] First operand with type of complex used to perform @f$*=@f$ operation.*/
+                       const RFLOAT x /**< [in] Second operand with type of RFLOAT used to perform @f$*=@f$ operation. */
                       )
 {
     a.dat[0] = a.dat[0] * x;
@@ -306,9 +312,9 @@ inline void operator*=(Complex& a,    /**< [in] First operand with type of compl
 }
 
 /**
- *  @brief Implement the div operation between a  complex number and a RFLOAT number, e.g. c = a / x, where a is a complex number and x is a RFLOAT number.
+ *  @brief Implement the div operation between a  complex number and a RFLOAT number, e.g. @f$c = a / x@f$, where @f$a@f$ is a complex number and @f$x@f$ is a RFLOAT number.
  *
- *  @return the result of c = a / x.
+ *  @return the result of @f$c = a / x@f$.
  */
 inline Complex operator/(const Complex a, /**< [in] First operand with type of complex used to perform div operation.*/
                          const RFLOAT x   /**< [in] Second operand with type of RFLOAT used to perform div operation. */
@@ -321,12 +327,12 @@ inline Complex operator/(const Complex a, /**< [in] First operand with type of c
 }
 
 /**
- *  @brief Implement the /= operation between a  complex number and a RFLOAT number, e.g. a /=  x, where a is a complex number and x is a RFLOAT number.
+ *  @brief Implement the @f$/=@f$ operation between a  complex number and a RFLOAT number, e.g. @f$a /=  x@f$, where @f$a@f$ is a complex number and @f$x@f$ is a RFLOAT number.
  *
- *  @return the result of a /= x.
+ *  @return the result of @f$a /= x@f$.
  */
-inline void operator/=(Complex& a,    /**< [in] First operand with type of complex used to perform /= operation.*/
-                       const RFLOAT x /**< [in] Second operand with type of RFLOAT used to perform /= operation. */
+inline void operator/=(Complex& a,    /**< [in] First operand with type of complex used to perform @f$/=@f$ operation.*/
+                       const RFLOAT x /**< [in] Second operand with type of RFLOAT used to perform @f$/=@f$ operation. */
                       )
 {
     a.dat[0] = a.dat[0] / x;
