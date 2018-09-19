796f56978a94c9964590cb3c4e2b696893267538
Huabin Ruan <ruanhuabin@tsinghua.edu.cn>
2018-09-19 16:40:40 +0800

    1. Change the initial letter of the comments of the argument from uppercase to lowercase in Precision.h. 2. Finish document for Precision.h 3. Add Parallel.h in Doxyfile. 4. Complete document for Parallel.h. 5. Add *.swo, *.swn in .gitignore for...
    Summary: ...preventing tracking temporary files generated by vim editor.
    
    Test Plan: NA
    
    Reviewers: hmx
    
    Subscribers: zhaohe, YuFande, yhk, xli, wangzhao, shenbo, qiaol, longx17, lisq, Johanli
    
    Differential Revision: http://124.65.131.150:11111/D34

--- include/Parallel.h
+++ include/Parallel.h
@@ -5,94 +5,103 @@
  * Execution:
  * Description:
  * ****************************************************************************/
+/** @file
+ *  @author Hongkun Yu
+ *  @author Mingxu Hu
+ *  @author Siyuan Ren
+ *  @author Huabin Ruan
+ *  @version 1.4.11.180919
+ *  @copyright THUNDER Non-Commercial Software License Agreement
+ *
+ *  ChangeLog
+ *  AUTHOR      | TIME       | VERSION       | DESCRIPTION
+ *  ------      | ----       | -------       | -----------
+ *  Huabin Ruan | 2018/09/13 | 1.4.11.080913 | Add header for file and functions
+ *
+ *  @brief Parallel.h encapsulates MPI related functions used for process partitioning, sending and receiving large size data, and so on.
+  */
 
 #ifndef PARALLEL_H
 #define PARALLEL_H
 
 #include <cstdio>
-
 #include <mpi.h>
-
 #include "Logging.h"
 #include "Precision.h"
-
 #include <boost/noncopyable.hpp>
 
 /**
- * The maximum buf size used in MPI environment. It is just a little bit smaller
- * than INT_MAX, and it can be divided by 2, 4, 8 and 16.
- * BTW, INT_MAX = 2147483647
+ * @brief the maximum buf size used in MPI environment. It is just a little bit smaller than INT_MAX(2147483647), and it can be divided by 2, 4, 8 and 16.
  */
 #define MPI_MAX_BUF 2000000000
 
 /**
- * rank ID of master process
+ * @brief process ID of master process
  */
 #define MASTER_ID 0
 
 /**
- * rank ID of the leader process of hemisphere A
+ * @brief process ID of the leader process of hemisphere A
  */
 #define HEMI_A_LEAD 1
 
 /**
- * rank IF of the leader process of hemisphere B
+ * @brief process ID of the leader process of hemisphere B
  */
 #define HEMI_B_LEAD 2
 
 /**
- * This macro is a short hand of a condition statement that the current process
- * is the master process.
+ * @brief This macro is a short hand of a condition statement that the current process is the master process.
  */
 #define IF_MASTER if (_commRank == MASTER_ID)
 
 /**
- * This macro is a short hand of a condition statement that the current process
- * is not the master process.
+ * @brief This macro is a short hand of a condition statement that the current process is not the master process.
  */
 #define NT_MASTER if (_commRank != MASTER_ID)
 
 /**
- * This macro is a short hand of logging when the current process is the
- * master process.
+ * @brief This macro is a short hand of logging when the current process is the master process.
  *
- * @param LEVEL  level of the log
- * @param LOGGER logger of the log
+ * [in] LEVEL  level of the log
+ * [in] param LOGGER logger of the log
  */
 #define MLOG(LEVEL, LOGGER) \
     IF_MASTER CLOG(LEVEL, LOGGER) << "MASTER: "
 
 /**
- * This macro is a short hand of logging when the current process is the
- * leader process of hemisphere A.
+ * @brief This macro is a short hand of logging when the current process is the leader process of hemisphere A.
  *
- * @param LEVEL  level of the log
- * @param LOGGER logger of the log
+ * [in] LEVEL  level of the log
+ * [in] LOGGER logger of the log
  */
 #define ALOG(LEVEL, LOGGER) \
     if (_commRank == HEMI_A_LEAD) CLOG(LEVEL, LOGGER) << "A_LEAD: "
 
 /**
- * This macro is a short hand of logging when the current process is the
- * leader process of hemisphere B.
+ * @brief This macro is a short hand of logging when the current process is the leader process of hemisphere B.
  *
- * @param LEVEL  level of the log
- * @param LOGGER logger of the log
+ * [in] LEVEL  level of the log
+ * [in] LOGGER logger of the log
  */
 #define BLOG(LEVEL, LOGGER) \
     if (_commRank == HEMI_B_LEAD) CLOG(LEVEL, LOGGER) << "B_LEAD: "
 
 /**
- * This macro is a short hand of logging when the current process is not the 
- * master process. Along with this log, the rank ID of the current process 
- * will be shown.
+ * @brief This macro is a short hand of logging when the current process is not the master process. Along with this log, the rank ID of the current process will be shown.
  *
- * @param LEVEL  level of the log
- * @param LOGGER logger of the log
+ * [in] LEVEL  level of the log
+ * [in] LOGGER logger of the log
  */
 #define ILOG(LEVEL, LOGGER) \
     NT_MASTER CLOG(LEVEL, LOGGER) << "RANK " << _commRank << ": "
 
+
+/**
+ *  @brief Define a macro used for logging memory usage information in thunder.
+ *  
+ *  [in] msg string prefix of the message.
+ */
 #define CHECK_MEMORY_USAGE(msg) \
     do \
     { \
@@ -103,180 +112,150 @@
 
 class Parallel: private boost::noncopyable
 {
-    protected:
+protected:
 
-        /**
-         * number of processes in MPI_COMM_WORLD
-         */
-        int _commSize;
+    /**
+     * @brief number of processes in MPI_COMM_WORLD
+     */
+    int _commSize;
 
-        /**
-         * the rank ID of the current process in MPI_COMM_WORLD
-         */
-        int _commRank;
+    /**
+     * @brief the rank ID of the current process in MPI_COMM_WORLD
+     */
+    int _commRank;
 
-        /**
-         * communicator of hemisphere A(B)
-         */
-        MPI_Comm _hemi;
+    /**
+     * @brief communicator of hemisphere A(B)
+     */
+    MPI_Comm _hemi;
 
-        MPI_Comm _slav;
+    MPI_Comm _slav;
 
-    public:
+public:
 
-        /**
-         * default constructor
-         */
-        Parallel();
+    /**
+     * default constructor
+     */
+    Parallel();
 
-        /**
-         * default deconstructor
-         */
-        ~Parallel();
+    /**
+     * default deconstructor
+     */
+    ~Parallel();
 
-        /**
-         * This function detects the number of processes in MPI_COMM_WORLD and
-         * the rank ID of the current process in MPI_COMM_WORLD. Moreover, it
-         * will assign all process in MPI_COMM_WORLD into three parts: master,
-         * hemisphere A and hemisphere B.
-         */
-        void setMPIEnv();
+    /**
+     * @brief This function detects the number of processes in MPI_COMM_WORLD and the rank ID of the current process in MPI_COMM_WORLD. Moreover, it will assign all process in MPI_COMM_WORLD into three parts: master, hemisphere A and hemisphere B.
+     */
+    void setMPIEnv();
 
-        /**
-         * This function inherits the MPI information by parameters.
-         *
-         * @param commSize the numbber of process in MPI_COMM_WORLD
-         * @param commRank the rank ID of the current process in MPI_COMM_WORLD
-         * @param hemi     the hemisphere of the current process
-         */
-        void setMPIEnv(const int commSize,
-                       const int commRank,
-                       const MPI_Comm& hemi,
-                       const MPI_Comm& slav);
+    /**
+     * @brief This function inherits the MPI information by parameters.
+     *
+     * @param commSize the numbber of process in MPI_COMM_WORLD
+     * @param commRank the rank ID of the current process in MPI_COMM_WORLD
+     * @param hemi     the hemisphere of the current process
+     */
+    void setMPIEnv(const int commSize,  /**< [in] the number of process in MPI_COMM_WORLD. */
+                  const int commRank,   /**< [in] the process ID of the current process in MPI_COMM_WORLD. */
+                  const MPI_Comm &hemi, /**< [in] the hemisphere of the current process. */
+                  const MPI_Comm &slav  /**< [in] the slave communitor. */
+                  );
 
-        /**
-         * This function returns whether the current process is the master
-         * process or not.
-         */
-        bool isMaster() const;
+    /**
+     * @brief Check whether the current process is the master process or not.
+     */
+    bool isMaster() const;
 
-        /**
-         * This function returns whether the current process is in hemisphere A
-         * or not.
-         */
-        bool isA() const;
+    /**
+     * @brief Check whether the current process is in hemisphere A or not.
+     */
+    bool isA() const;
 
-        /**
-         * This function returns whether the current process is in hemisphere B
-         * or not.
-         */
-        bool isB() const;
+    /**
+     * @brief Check whether the current process is in hemisphere B or not.
+     */
+    bool isB() const;
 
-        /**
-         * This function returns the number of processes in MPI_COMM_WORLD.
-         */
-        int commSize() const;
+    /**
+     * @brief Get the number of processes in MPI_COMM_WORLD.
+     */
+    int commSize() const;
 
-        /**
-         * This function sets the number of processes in MPI_COMM_WORLD.
-         *
-         * @param commSize the number of processes in MPI_COMM_WORLD
-         */
-        void setCommSize(const int commSize);
+    /**
+     * @brief Sets the number of processes in MPI_COMM_WORLD.
+     *
+     */
+    void setCommSize(const int commSize /**< [in] the number of processes in MPI_COMM_WORLD. */);
 
-        /**
-         * This function returns the rank ID of the current process in
-         * MPI_COMM_WORLD.
-         */
-        int commRank() const;
+    /**
+     * @brief Get the process ID of the current process in MPI_COMM_WORLD.
+     */
+    int commRank() const;
 
-        /**
-         * This function sets the rank ID of the current process in
-         * MPI_COMM_WORLD.
-         *
-         * @param commRank the rank ID of the current process in MPI_COMM_WORLD
-         */
-        void setCommRank(const int commRank);
+    /**
+     * @brief Sets the process ID of the current process in MPI_COMM_WORLD.
+     *
+     */
+    void setCommRank(const int commRank /**< [in] process ID to be set. */);
 
-        /**
-         * This function returns the hemisphere of the current process.
-         */
-        MPI_Comm hemi() const;
+    /**
+     * @brief Get the hemisphere of the current process.
+     */
+    MPI_Comm hemi() const;
 
-        /**
-         * This function sets the hemisphere of the current process.
-         *
-         * @param hemi the hemisphere of the current process
-         */
-        void setHemi(const MPI_Comm& hemi);
+    /**
+     * @brief Sets the hemisphere of the current process.
+     */
+    void setHemi(const MPI_Comm &hemi /**< [in] the hemisphere that the current process belongs to. */);
 
-        MPI_Comm slav() const;
 
-        void setSlav(const MPI_Comm& slav);
+
+    /**
+     *  @brief Get the communitor that the slave process belongs to.
+     */
+    MPI_Comm slav() const;
+
+
+    /**
+     *  @brief Set the communicator for the slave process.
+     */
+    void setSlav(const MPI_Comm &slav /**< [in] the communicator for the slave process.*/);
 };
 
-/**
- * This function displays MPI information of a Parallel object.
- *
- * @param parallel a Parallel object
- */
-void display(const Parallel& parallel);
 
 /**
- * This function is an overwrite of MPI_Recv function for large data
- * transporting.
- *
- * @param buf      the buffer area for receiving data
- * @param count    the number of the data
- * @param datatype the type of the data
- * @param source   the rank ID of the sending process in the communicator
- * @param tag      the tag of the data
- * @param comm     the communicator
+ * @brief This function can be used for receiving large size(>2GB) data. 
  */
-void MPI_Recv_Large(void* buf,
-                    size_t count,
-                    MPI_Datatype datatype,
-                    int source,
-                    int tag,
-                    MPI_Comm comm);
+void MPI_Recv_Large(void *buf,             /**< [out] the data buffer used to receive data. */
+                    size_t count,          /**< [in]  the number of data elements to receive. */
+                    MPI_Datatype datatype, /**< [in]  the type of the received data elements. */
+                    int source,            /**< [in]  the process ID of the sending process in the communicator. */
+                    int tag,               /**< [in]  the unique tag used for identifying the data. */
+                    MPI_Comm comm          /**< [in]  the communicator that the sending/receiving processes belongs to. */
+                   );
 
 /**
- * This function is an overwrite of MPI_Ssend function for large data
- * transporting.
- *
- * @param buf      the buffer area for sending data
- * @param count    the number of the data
- * @param datatype the type of the data
- * @param dest     the rank ID of the receiving process in the communicator
- * @param tag      the tag of the data
- * @param comm     the communicator
+ * @brief This function can be used for sending large size(>2GB) data. 
  */
-void MPI_Ssend_Large(const void* buf,
-                     size_t count,
-                     MPI_Datatype datatype,
-                     int dest,
-                     int tag,
-                     MPI_Comm comm);
+void MPI_Ssend_Large(const void *buf,       /**< [in] the data elements to be sent. */
+                     size_t count,          /**< [in] the number of data elements to be sent. */
+                     MPI_Datatype datatype, /**< [in] the type of the  data elements to be sent. */
+                     int dest,              /**< [in] the process ID of the received process in the communicator. */
+                     int tag,               /**< [in] the unique tag used for identifying the data. */
+                     MPI_Comm comm          /**< [in] the communicator that the sending/receiving processes belongs to. */
+                    );
 
 /**
- * This function is an overwrite of MPI_Bcast function for large data
- * transporting.
- *
- * @param buf      the buffer area for broadcasting data
- * @param count    the number of the data
- * @param datatype the type of the data
- * @param dest     the rank ID of the root process in the communicator
- * @param comm     the communicator
+ * @brief  This function can be used for broadcasting large size(>2GB) data
  */
-void MPI_Bcast_Large(void* buf,
-                     size_t count,
-                     MPI_Datatype datatype,
-                     int root,
-                     MPI_Comm comm);
-
+void MPI_Bcast_Large(void *buf,             /**< [in] the data elements used for broadcasting. */
+                     size_t count,          /**< [in] the number of data elements to be broadcasted. */
+                     MPI_Datatype datatype, /**< [in] the type of the  data elements to be broadcasted. */
+                     int root,              /**< [in] the process ID of the broadcasting process in the communicator. */
+                     MPI_Comm comm          /**< [in] the communicator that the sending/receiving processes belongs to. */
+                     );
 /**
- * This function is an overwrite of MPI_Allreduce function for large data
- * transporting.
+ * @brief This function is used for all reducing operation for large size(>2GB) data
  *
  * @param buf      the buffer area of all-reducing data
  * @param count    the number of the data
@@ -284,10 +263,11 @@ void MPI_Bcast_Large(void* buf,
  * @param op       the operator of all-reducing
  * @param comm     the communicator
  */
-void MPI_Allreduce_Large(void* buf,
-                         size_t count,
-                         MPI_Datatype datatype,
-                         MPI_Op op,
-                         MPI_Comm comm);
+void MPI_Allreduce_Large(void *buf,             /**< [in] the data buffer used for saving all reducing result. */
+                         size_t count,          /**< [in] the number of data elements to be reduced. */
+                         MPI_Datatype datatype, /**< [in] the type of the  data elements to be reduced. */
+                         MPI_Op op,             /**< [in] the operation to be performed in all reducing */ 
+                         MPI_Comm comm        /**< [in] the communicator that the all reducing processes belongs to. */
+                        );
 
 #endif // PARALLEL_H
