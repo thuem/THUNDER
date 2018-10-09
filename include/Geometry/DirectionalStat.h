44299f63a5b2bf9867d506ab686eda3714504304
Zhao Wang <1067732876@qq.com>
2018-10-09 10:25:57 +0800

    DirectionalStat.h documentation done
    Summary: DirectionalStat.h documentation done
    
    Test Plan: NA
    
    Reviewers: hmx, ruanhuabin
    
    Reviewed By: hmx, ruanhuabin
    
    Differential Revision: http://124.65.131.150:11111/D55

--- include/Geometry/DirectionalStat.h
+++ include/Geometry/DirectionalStat.h
@@ -1,12 +1,20 @@
-/*******************************************************************************
- * Author: Hongkun Yu, Mingxu Hu
- * Dependency:
- * Test:
- * Execution:
- * Description:
+/** @file
+ *  @author Mingxu Hu
+ *  @author Zhao Wang
+ *  @version 1.4.11.080927
+ *  @copyright THUNDER Non-Commercial Software License Agreement
  *
- * Manual:
- * ****************************************************************************/
+ *  ChangeLog
+ *  AUTHOR    | TIME       | VERSION       | DESCRIPTION
+ *  ------    | ----       | -------       | -----------
+ *  Mingxu Hu | 2015/03/23 | 0.0.1.050323  | new file
+ *  Zhao Wang | 2018/09/27 | 1.4.11.080927 | add documentation 
+ *  
+ *  @brief DirectionStat.h contains several functions to carry out computations in Angular Central Gaussian and von Mises Distribution respectively.
+ *
+ *  The suffix "ACG" and "VMS" in the function names denotes two modes of distributions, "Angular Central Gaussian Distribution" and "von Mises Distribution". The prefix represents the computation to be executed. "pdf" returns the probability density function; "sample" implements sampling operation; "infer" gives the parameter matrix inference.
+ */
+
 
 #ifndef DIRECTIONAL_STAT_H
 #define DIRECTIONAL_STAT_H
@@ -27,172 +35,163 @@
 #include "Functions.h"
 
 /**
- * Probabilty Density Function of Angular Central Gaussian Distribution
- *
- * @param x   a quaternion
- * @param sig a symmetric positive definite parameter matrix
+ * @brief Calculate the probability density function of angular central Gaussian distribution.
  */
-double pdfACG(const dvec4& x,
-              const dmat44& sig);
+double pdfACG(const dvec4& x,       /**< [in] a quaternion */
+              const dmat44& sig     /**< [in] a symmetric positive definite parameter matrix */
+              );
 
 /**
- * Probability Density Function of Angular Central Gaussian Distribution
- *
- * paramter matrxix:
- * k0 0  0  0
- * 0  k1 0  0
- * 0  0  k1 0
- * 0  0  0  k1 
- *
- * @param x q quaterion
- * @param k0 the first paramter
- * @param k1 the second parameter
+ * @brief Calculate the probability density function of angular central Gaussian distribution. The parameter matrix of this ACG distribution is
+ * \f[
+ *   \begin{pmatrix}
+ *       k0 & 0  & 0  & 0 \\
+ *       0  & k1 & 0  & 0 \\
+ *       0  & 0  & k1 & 0 \\
+ *       0  & 0  & 0  & k1 
+ *   \end{pmatrix}
+ * \f]
  */
-double pdfACG(const dvec4& x,
-              const double k0,
-              const double k1);
+double pdfACG(const dvec4& x,      /**< [in] a quaternion */
+              const double k0,     /**< [in] first parameter of a positive-definite matrix */
+              const double k1      /**< [in] second parameter of a positive-definite matrix */
+              );
 
 /**
- * Sample from an Angular Central Gaussian Distribution
- *
- * @param dst the destination table
- * @param src the symmetric positive definite parameter matrix
- * @param n   the number of samples
+ * @brief Sample from an angular central Gaussian distribution.
  */
-void sampleACG(dmat4& dst,
-               const dmat44& src,
-               const int n);
+void sampleACG(dmat4& dst,         /**< [in] the destination table */
+               const dmat44& src,  /**< [in] the symmetric positive definite parameter matrix */
+               const int n         /**< [in] the number of samples */
+               );
 
 /**
- * Sample from an Angular Central Gaussian Distribution
- *
- * paramter matrix:
- * k0 0  0  0
- * 0  k1 0  0
- * 0  0  k1 0
- * 0  0  0  k1 
- *
- * @param dst the destination table
- * @param k0  the first parameter
- * @param k1  the second parameter
- * @param n   the number of samples
+ * @brief Sample from an angular central Gaussian distribution. The parameter matrix of this ACG distribution is
+ * \f[
+ *   \begin{pmatrix}
+ *       k0 & 0  & 0  & 0 \\
+ *       0  & k1 & 0  & 0 \\
+ *       0  & 0  & k1 & 0 \\
+ *       0  & 0  & 0  & k1 
+ *   \end{pmatrix}
+ * \f]
  */
-void sampleACG(dmat4& dst,
-               const double k0,
-               const double k1,
-               const int n);
+void sampleACG(dmat4& dst,         /**< [in] the destination table */
+               const double k0,    /**< [in] first parameter of a positive-definite matrix */
+               const double k1,    /**< [in] second parameter of a positive-definite matrix */
+               const int n         /**< [in] the number of samples */
+               );
 
 /**
- * Sample from an Angular Central Gaussian Distribution
- *
- * parameter matrix:
- * 1 0 0 0
- * 0 k1 0 0
- * 0 0 k2 0
- * 0 0 0 k3
- *
- * @param dst the destination table
- * @param k1  the 1st parameter
- * @param k2  the 2nd parameter
- * @param k3  the 3rd parameter
- * @param n   the number of samples
+ * @brief Sample from an angular central Gaussian distribution. The parameter matrix of this ACG distribution is
+ * \f[
+ *   \begin{pmatrix}
+ *       1 & 0  & 0  & 0 \\
+ *       0 & k1 & 0  & 0 \\
+ *       0 & 0  & k2 & 0 \\
+ *       0 & 0  & 0  & k3 
+ *   \end{pmatrix}
+ * \f]
  */
-void sampleACG(dmat4& dst,
-               const double k1,
-               const double k2,
-               const double k3,
-               const int n);
+void sampleACG(dmat4& dst,        /**< [in] the destination table */
+               const double k1,   /**< [in] first parameter of a positive-definite matrix */
+               const double k2,   /**< [in] second parameter of a positive-definite matrix */
+               const double k3,   /**< [in] third parameter of a positive-definite matrix */
+               const int n        /**< [in] the number of samples */
+               );
 
 /**
- * Paramter Matrix Inference from Data Assuming the Distribution Follows an
- * Angular Central Gaussian Distribution
- *
- * @param dst the paramter matrix
- * @param src the data
+ * @brief Calculate the parameter matrix inference from source data assuming the distribution follows an angular central Gaussian distribution.
  */
-void inferACG(dmat44& dst,
-              const dmat4& src);
+void inferACG(dmat44& dst,       /**< [in] the parameter matrix */
+              const dmat4& src   /**< [in] the given data */
+              );
 
 /**
- * Parameter Inference from Data Assuming the Distribution Follows an Angular
- * Central Gaussian Distribution
- *
- * @param k0  the first parameter
- * @param k1  the second paramter
- * @param src the data
+ * @brief Calculate the parameter matrix inference from source data assuming the distribution follows an angular central Gaussian distribution.
  */
-void inferACG(double& k0,
-              double& k1,
-              const dmat4& src);
-
-void inferACG(double& k,
-              const dmat4& src);
-
-void inferACG(double& k1,
-              double& k2,
-              double& k3,
-              const dmat4& src);
+void inferACG(double& k0,       /**< [in] first parameter of a positive-definite matrix */
+              double& k1,       /**< [in] second parameter of a positive-definite matrix */
+              const dmat4& src  /**< [in] the given data */
+              );
 
 /**
- * Parameter Inference from Data Assuming the Distribution Follows an Angular
- * Central Gaussian Distribution
- *
- * @param mean the mean of ACG distribution
- * @param src  the data
+ * @brief Calculate the parameter matrix inference from source data assuming the distribution follows an angular central Gaussian distribution.
  */
-void inferACG(dvec4& mean,
-              const dmat4& src);
+void inferACG(double& k,        /**< [in]  */
+              const dmat4& src  /**< [in] the given data */
+              );
 
 /**
- * Probabilty Density Function of von Mises Distribution M(mu, kappa)
- *
- * @param x     the orientation in unit vector
- * @param mu    the mode of the von Mises distribution in unit vector
- * @param kappa the concnetration parameter of the von Mises distribution
+ * @brief Calculate the parameter matrix inference from source data assuming the distribution follows an angular central Gaussian distribution.
  */
-double pdfVMS(const dvec2& x,
-              const dvec2& mu,
-              const double k);
+void inferACG(double& k1,       /**< [in] first parameter of a positive-definite matrix */
+              double& k2,       /**< [in] second parameter of a positive-definite matrix */
+              double& k3,       /**< [in] third parameter of a positive-definite matrix */
+              const dmat4& src  /**< [in] the given data */
+              );
 
 /**
- * Sample from von Mises Distribution M(mu, kappa), the algorithm is from Best &
- * Fisher (1979)
- *
- * @param dst   the destination table
- * @param mu    the mode of the von Mises distribution
- * @param kappa the concentration parameter of the von Mises distribution
- * @param n     number of sample
+ * @brief Calculate the parameter matrix inference from source data assuming the distribution follows an angular central Gaussian distribution.
  */
-void sampleVMS(dmat2& dst,
-               const dvec2& mu,
-               const double k,
-               const double n);
-
-void sampleVMS(dmat4& dst,
-               const dvec4& mu,
-               const double k,
-               const double n);
+void inferACG(dvec4& mean,      /**< [in] the mean of ACG distribution */
+              const dmat4& src  /**< [in] the given data */
+              );
 
 /**
- * Mode and Concentration Paramter Inference from Data Assuming the Distribution
- * Follows a von Mises Distribution
- *
- * @param mu    the mode of the von Mises distribution
- * @param kappa the concentration paramter of the von Mises distribution
- * @param src    the data
+ * @brief Calculate the probability density function of von Mises Distribution M(mu, kappa).
  */
-void inferVMS(dvec2& mu,
-              double& k,
-              const dmat2& src);
+double pdfVMS(const dvec2& x,   /**< [in] the orientation in unit vector */
+              const dvec2& mu,  /**< [in] the mode of the von Mises distribution in unit vector */
+              const double k    /**< [in] the concentration parameter of the von Mises distribution */
+              );
 
-void inferVMS(double& k,
-              const dmat2& src);
+/**
+ * @brief Sample from von Mises Distribution M(mu, kappa), the algorithm is from Best & Fisher (1979).
+ */
+void sampleVMS(dmat2& dst,      /**< [in] the destination table */
+               const dvec2& mu, /**< [in] the mode of the von Mises distribution */
+               const double k,  /**< [in] the concentration parameter of the von Mises distribution */
+               const double n   /**< [in] the number of samples */
+               );
 
-void inferVMS(dvec4& mu,
-              double& k,
-              const dmat4& src);
+/**
+ * @brief Sample from von Mises Distribution M(mu, kappa), the algorithm is from Best & Fisher (1979).
+ */
+void sampleVMS(dmat4& dst,      /**< [in] the destination table */
+               const dvec4& mu, /**< [in] the mode of the von Mises distribution */
+               const double k,  /**< [in] the concentration parameter of the von Mises distribution */
+               const double n   /**< [in] the number of samples */
+               );
 
-void inferVMS(double& k,
-              const dmat4& src);
+/**
+ * @brief Calculate the mode and concentration parameter inference from the given data assuming the distribution follows a von Mises Distribution.
+ */
+void inferVMS(dvec2& mu,        /**< [in] the mode of the von Mises distribution */
+              double& k,        /**< [in] the concentration parameter of the von Mises distribution */
+              const dmat2& src  /**< [in] the given data */
+              );
+
+/**
+ * @brief Calculate the mode and concentration parameter inference from the given data assuming the distribution follows a von Mises Distribution.
+ */
+void inferVMS(double& k,        /**< [in] the concentration parameter of the von Mises distribution */
+              const dmat2& src  /**< [in] the given data */
+              );
+
+/**
+ * @brief Calculate the mode and concentration parameter inference from the given data assuming the distribution follows a von Mises Distribution.
+ */
+void inferVMS(dvec4& mu,        /**< [in] the mode of the von Mises distribution */
+              double& k,        /**< [in] the concentration parameter of the von Mises distribution */
+              const dmat4& src  /**< [in] the given data */
+              );
+
+/**
+ * @brief Calculate the mode and concentration parameter inference from the given data assuming the distribution follows a von Mises Distribution.
+ */
+void inferVMS(double& k,        /**< [in] the concentration parameter of the von Mises distribution */
+              const dmat4& src  /**< [in] the given data */
+              );
 
 #endif // DIRECTIONAL_STAT_H
